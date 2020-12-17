5b9d8e6129d55206dbb66f434a2d421c159de26f
Blake Drumm <v-bldrum@microsoft.com>
2020-12-17 13:08:31 -0500

    Update OMCertCheck.ps1

--- Powershell/OMCertCheck.ps1
+++ Powershell/OMCertCheck.ps1
@@ -44,7 +44,7 @@
 param
 (
 	[Parameter(Mandatory = $false,
-				Position = 2)]
+			   Position = 2)]
 	[String]$Output = "C:\Windows\temp\SCOM-CertChecker-Output.txt",
 	[Parameter(Position = 1)]
 	[Array]$Servers
@@ -71,8 +71,8 @@ else
 # START FUNCTION
 function SCOM-CertCheck
 {
-    [OutputType([string])]
-    [CmdletBinding()]
+	[OutputType([string])]
+	[CmdletBinding()]
 	param
 	(
 		[Parameter(Mandatory = $false,
@@ -81,25 +81,25 @@ function SCOM-CertCheck
 		[Parameter(Position = 2)]
 		[Array]$Servers
 	)
-    if($null -eq $Servers){$Servers = $env:COMPUTERNAME}
-    else{
-    Write-Host $Servers
-    $Servers = ($Servers.Split(",").Split(" ") -replace (" ", ""))
-    pause
-	$Servers = $Servers | select -Unique
-    }
+	if ($null -eq $Servers) { $Servers = $env:COMPUTERNAME }
+	else
+	{
+		Write-Host $Servers
+		$Servers = ($Servers.Split(",").Split(" ") -replace (" ", ""))
+		pause
+		$Servers = $Servers | select -Unique
+	}
 	[string[]]$out = @()
 	foreach ($server in $Servers)
 	{
 		Write-Output " "
 		Write-Output "Certificate Checker"
-		Invoke-Command -ComputerName $server {
-            Function Time-Stamp
-            {
-		
-	            $TimeStamp = Get-Date -Format "MM/dd/yyyy hh:mm:ss tt"
-	            return $TimeStamp
-            }
+			Function Time-Stamp
+			{
+				
+				$TimeStamp = Get-Date -Format "MM/dd/yyyy hh:mm:ss tt"
+				return $TimeStamp
+			}
 			# Consider all certificates in the Local Machine "Personal" store
 			$certs = [Array] (dir cert:\LocalMachine\my\)
 			$out = [Array] @()
@@ -503,108 +503,13 @@ Enhanced Key Usage Extension
 
 $time : Script Completed
 "@
-            $out | Out-File $Output
+				$out | Out-File $Output
 			}
 			start C:\Windows\explorer.exe -ArgumentList "/select, $Output"
 			#return $out
-            exit 0
-		}
+			exit 0
+		
 	}
 }
-if($null -eq $Servers){ SCOM-CertCheck }else{ SCOM-CertCheck -Servers $Servers }
-
-		
-# SIG # Begin signature block
-# MIIRKwYJKoZIhvcNAQcCoIIRHDCCERgCAQExDzANBglghkgBZQMEAgEFADB5Bgor
-# BgEEAYI3AgEEoGswaTA0BgorBgEEAYI3AgEeMCYCAwEAAAQQH8w7YFlLCE63JNLG
-# KX7zUQIBAAIBAAIBAAIBAAIBADAxMA0GCWCGSAFlAwQCAQUABCA8zLMfFvYZZvfY
-# o9sm71sqJ58sQZektSQ75vNwS+DBfaCCDB4wggM3MIICI6ADAgECAhBid7E8Kyy6
-# okrKDBZQBZ7/MAkGBSsOAwIdBQAwGTEXMBUGA1UEAxMOQW50aG9ueSBEdWd1aWQw
-# IBcNMTcwNTI0MDIxNjI2WhgPMjE3NDAxMTUxNDAwMDBaMBkxFzAVBgNVBAMTDkFu
-# dGhvbnkgRHVndWlkMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3NXP
-# 0qsalOagDYU6g7VAQqeqZYlICq+yClVDSBb+3DNSFIy8M2IogpocMT5wAwOvnBBO
-# Yz34mgjqc03B7/uvXFDva1PW/4gRwL5jC+PJxg/FG4ghD+fRlKCDYC/55nNos04D
-# P2APbkO1VLo9LjCVQRUfXjtbifOWtMHOzcw/AzY5PuxzLc5YCPXdXBCMw94aL4jX
-# ABdkXp2o3kO9opsWkeZpG7VPsnGe0Ov4A0kHo5/33wVtNOWWQKDD8qJA/wZLDmKA
-# aQX60/hJKzzV7P7hNKoWmUxBAILe/N/soprlj8m5Li+Avx82XumDyH8LPyUad2YL
-# oNcsAZFcNokt3S+QlQIDAQABo4GAMH4wDwYDVR0TAQH/BAUwAwIBADAfBgNVHSUE
-# GDAWBggrBgEFBQcDAwYKKwYBBAGCNwoDDTBKBgNVHQEEQzBBgBA0bMynuhE47c/y
-# Vt4RcHC7oRswGTEXMBUGA1UEAxMOQW50aG9ueSBEdWd1aWSCEGJ3sTwrLLqiSsoM
-# FlAFnv8wCQYFKw4DAh0FAAOCAQEALkbCgE0Z3XNqhtD+COhaMyN+CiifirdRVUwL
-# jbwNjlq50x3MPx8Ty6/xs8W8nlM5Gl84S2LKDHYf/ycR/rR4L5BAR7HX1dLzVrbU
-# DvoFA1r2vCwUdk0fODBASIb1reort7EyBX8ofE4neW6J6uMrvy8IGmWH4caAJ9jU
-# o5pStCgn8aDvXS9Fx3nW+wwU3vUzcQYXXolnh0/EzB049/3KPhPQkWbZoxTRGSRb
-# VnhrGIcvqLJN8fm5flG343Om32QDnzmEsQIWkjq8hiQXt4oIyLuoQoEcK52zPJ6R
-# A44Icpr6kqYx/zjBrVdWL04cdNIFHles4CiiuOfkfIuxtXp+8DCCBBUwggL9oAMC
-# AQICCwQAAAAAATGJxlAEMA0GCSqGSIb3DQEBCwUAMEwxIDAeBgNVBAsTF0dsb2Jh
-# bFNpZ24gUm9vdCBDQSAtIFIzMRMwEQYDVQQKEwpHbG9iYWxTaWduMRMwEQYDVQQD
-# EwpHbG9iYWxTaWduMB4XDTExMDgwMjEwMDAwMFoXDTI5MDMyOTEwMDAwMFowWzEL
-# MAkGA1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExMTAvBgNVBAMT
-# KEdsb2JhbFNpZ24gVGltZXN0YW1waW5nIENBIC0gU0hBMjU2IC0gRzIwggEiMA0G
-# CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCqm47DqxFRJQG2lpTiT9jBCPZGI9lF
-# xZWXW6sav9JsV8kzBh+gD8Y8flNIer+dh56v7sOMR+FC7OPjoUpsDBfEpsG5zVvx
-# HkSJjv4L3iFYE+5NyMVnCxyys/E0dpGiywdtN8WgRyYCFaSQkal5ntfrV50rfCLY
-# FNfxBx54IjZrd3mvr/l/jk7htQgx/ertS3FijCPxAzmPRHm2dgNXnq0vCEbc0oy8
-# 9I50zshoaVF2EYsPXSRbGVQ9JsxAjYInG1kgfVn2k4CO+Co4/WugQGUfV3bMW44E
-# Tyyo24RQE0/G3Iu5+N1pTIjrnHswJvx6WLtZvBRykoFXt3bJ2IAKgG4JAgMBAAGj
-# gegwgeUwDgYDVR0PAQH/BAQDAgEGMBIGA1UdEwEB/wQIMAYBAf8CAQAwHQYDVR0O
-# BBYEFJIhp0qVXWSwm7Qe5gA3R+adQStMMEcGA1UdIARAMD4wPAYEVR0gADA0MDIG
-# CCsGAQUFBwIBFiZodHRwczovL3d3dy5nbG9iYWxzaWduLmNvbS9yZXBvc2l0b3J5
-# LzA2BgNVHR8ELzAtMCugKaAnhiVodHRwOi8vY3JsLmdsb2JhbHNpZ24ubmV0L3Jv
-# b3QtcjMuY3JsMB8GA1UdIwQYMBaAFI/wS3+oLkUkrk1Q+mOai97i3Ru8MA0GCSqG
-# SIb3DQEBCwUAA4IBAQAEVoJKfNDOyb82ZtG+NZ6TbJfoBs4xGFn5bEFfgC7AQiW4
-# GMf81LE3xGigzyhqA3RLY5eFd2E71y/j9b0zopJ9ER+eimzvLLD0Yo02c9EWNvG8
-# Xuy0gJh4/NJ2eejhIZTgH8Si4apn27Occ+VAIs85ztvmd5Wnu7LL9hmGnZ/I1JgF
-# snFvTnWu8T1kajteTkamKl0IkvGj8x10v2INI4xcKjiV0sDVzc+I2h8otbqBaWQq
-# taai1XOv3EbbBK6R127FmLrUR8RWdIBHeFiMvu8r/exsv9GU979Q4HvgkP0gGHgY
-# Il0ILowcoJfzHZl9o52R0wZETgRuehwg4zbwtlC5MIIExjCCA66gAwIBAgIMJFS4
-# fx4UU603+qF4MA0GCSqGSIb3DQEBCwUAMFsxCzAJBgNVBAYTAkJFMRkwFwYDVQQK
-# ExBHbG9iYWxTaWduIG52LXNhMTEwLwYDVQQDEyhHbG9iYWxTaWduIFRpbWVzdGFt
-# cGluZyBDQSAtIFNIQTI1NiAtIEcyMB4XDTE4MDIxOTAwMDAwMFoXDTI5MDMxODEw
-# MDAwMFowOzE5MDcGA1UEAwwwR2xvYmFsU2lnbiBUU0EgZm9yIE1TIEF1dGhlbnRp
-# Y29kZSBhZHZhbmNlZCAtIEcyMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC
-# AQEA2XhhoZauEv+j/yf2RGB7alYtZ+NfnzGSKkjt+QWEDm1OIlbK2JmXjmnKn3sP
-# CMgqK2jRKGErn+Qm7rq497DsXmob4li1tL0dCe3N6D3UZv++IiJtNibPEXiX6VUA
-# KMPpN069GeUXhEiyHCGt7HPS86in6V/oNc6FE6cim6yC6f7xX8QSWrH3DEDm0qDg
-# TWjQ7QwMEB2PBV9kVfm7KEcGDNgGPzfDJjYljHsPJ4hcODGlAfZeZN6DwBRc4OfS
-# XsyN6iOAGSqzYi5gx6pn1rNA7lJ/Vgzv2QXXlSBdhRVAz16RlVGeRhoXkb7BwAd1
-# skv3NrrFVGxfihv7DShhyInwFQIDAQABo4IBqDCCAaQwDgYDVR0PAQH/BAQDAgeA
-# MEwGA1UdIARFMEMwQQYJKwYBBAGgMgEeMDQwMgYIKwYBBQUHAgEWJmh0dHBzOi8v
-# d3d3Lmdsb2JhbHNpZ24uY29tL3JlcG9zaXRvcnkvMAkGA1UdEwQCMAAwFgYDVR0l
-# AQH/BAwwCgYIKwYBBQUHAwgwRgYDVR0fBD8wPTA7oDmgN4Y1aHR0cDovL2NybC5n
-# bG9iYWxzaWduLmNvbS9ncy9nc3RpbWVzdGFtcGluZ3NoYTJnMi5jcmwwgZgGCCsG
-# AQUFBwEBBIGLMIGIMEgGCCsGAQUFBzAChjxodHRwOi8vc2VjdXJlLmdsb2JhbHNp
-# Z24uY29tL2NhY2VydC9nc3RpbWVzdGFtcGluZ3NoYTJnMi5jcnQwPAYIKwYBBQUH
-# MAGGMGh0dHA6Ly9vY3NwMi5nbG9iYWxzaWduLmNvbS9nc3RpbWVzdGFtcGluZ3No
-# YTJnMjAdBgNVHQ4EFgQU1Ie4jeblQDydWgZjxkWE2d27HMMwHwYDVR0jBBgwFoAU
-# kiGnSpVdZLCbtB7mADdH5p1BK0wwDQYJKoZIhvcNAQELBQADggEBACRyUKUMvEAJ
-# psH01YJqTkFfzseIOdPkfPkibDh4uPS692vhJOudfM1IrIvstXZMj9yCaQiW57rh
-# Z7bwpr8YCELh680ZWDmlEWEj1hnXAOm70vlfQfsEPv6KIGAM0U8jWhkaGO/Yxt7W
-# X1ShepPhtneFwPuxRsQJri9T+5WcjibiSuTE5jw177rG2bnFzc0Hm2O7PQ9hvFV8
-# IxC1jIqj0mhFsUC6oN08GxVAuEl4b+WUwG1WSzz2EirUhfNIEwXhuzBFCkG3fJJu
-# vk6SYILKW2TmVdPSB96dX5uhAe2b8MNduxnwGAyaoBzpaggLPelml6d1Hg+/KNcJ
-# Iw3iFvq68zQxggRjMIIEXwIBATAtMBkxFzAVBgNVBAMTDkFudGhvbnkgRHVndWlk
-# AhBid7E8Kyy6okrKDBZQBZ7/MA0GCWCGSAFlAwQCAQUAoEwwGQYJKoZIhvcNAQkD
-# MQwGCisGAQQBgjcCAQQwLwYJKoZIhvcNAQkEMSIEIDpL+JNiEED7Hy6+nr3Y73uZ
-# 4UlWhy5pfqt2ZhPB8ayCMA0GCSqGSIb3DQEBAQUABIIBAAmqKkBGbV45eKBnexS8
-# 1/eorvxrqGpKGxf8dEOHdWImjAMmxUqh0Xz2VruffgxBBV0c2zJfghOU6tsTk0fM
-# j6nPAfm7TdhoFxVQoM20kDAmCj5frk08etYu1W6hYLxn7vQeQ3D3qB+N05N/GAUu
-# kEUn/K/BNcyi/LNliudamACsv5XeYaOEUCzFwxt/+HGTt2/1TL6spQW4SQp1557W
-# RlPLrw6hWv2yaQq/SYiecccfrUxenN6NsF4jBYNvNfg63QTbiWd6AGZtC4VqH1af
-# xqmSEhP2qN94nEhK+vuXGJC06wfYF+oswESYmX4073bkWkFgmfhgaNPfD07ZKToH
-# AHShggK5MIICtQYJKoZIhvcNAQkGMYICpjCCAqICAQEwazBbMQswCQYDVQQGEwJC
-# RTEZMBcGA1UEChMQR2xvYmFsU2lnbiBudi1zYTExMC8GA1UEAxMoR2xvYmFsU2ln
-# biBUaW1lc3RhbXBpbmcgQ0EgLSBTSEEyNTYgLSBHMgIMJFS4fx4UU603+qF4MA0G
-# CWCGSAFlAwQCAQUAoIIBDDAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqG
-# SIb3DQEJBTEPFw0yMDA3MTYwNjE4NTdaMC8GCSqGSIb3DQEJBDEiBCDGeuMIBf4p
-# R8Mue9iyw6rJ0iABVT+yA+v4WHkHYCQb1zCBoAYLKoZIhvcNAQkQAgwxgZAwgY0w
-# gYowgYcEFD7HZtXU1HLiGx8hQ1IcMbeQ2UtoMG8wX6RdMFsxCzAJBgNVBAYTAkJF
-# MRkwFwYDVQQKExBHbG9iYWxTaWduIG52LXNhMTEwLwYDVQQDEyhHbG9iYWxTaWdu
-# IFRpbWVzdGFtcGluZyBDQSAtIFNIQTI1NiAtIEcyAgwkVLh/HhRTrTf6oXgwDQYJ
-# KoZIhvcNAQEBBQAEggEA05/d+OWM6olGN1Qp1+f9grZLpxzQaVL3LsrMwLiZW3rX
-# yqRdOsC23WSs00g8YzazobOxfLi2y/teDFOtaMwmqBM07N4c7cSaZg0YAHWeNmyr
-# d63jykKUo4keqXRNwQ84drZXncjNNzeemUrWSn7IEb6bIr85KCBaTm4Ca7p+bfdQ
-# A6y5yW9MaWE0Jmcp3eHvGbx2ZaLwYq6tiGY2gBGb7TMKFBurFyAmIo31zwFQxKv/
-# iU/enr67uh7Ki8iEW2MP7EZsbq3u8jTsY7sflfwQvGRE2rZpHreGefAu4ZJrOX1M
-# D9ABuPRWKalZz+oebX+EXqUUaJI0qUVsPrOJfFuIZg==
-# SIG # End signature block
+if ($null -eq $Servers) { SCOM-CertCheck }
+else { SCOM-CertCheck -Servers $Servers }
