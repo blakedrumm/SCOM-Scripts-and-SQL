8a534140fd015eb4f6d4ba41c532e26f9f38f678
Blake Drumm <v-bldrum@microsoft.com>
2021-01-02 01:45:56 -0500

    Update Start-SCOMDiscovery.ps1

--- Powershell/Start-SCOMDiscovery.ps1
+++ Powershell/Start-SCOMDiscovery.ps1
@@ -5,7 +5,16 @@ param
 	[String]$ManagementServer,
 	[Parameter(Mandatory = $false,
 			   Position = 2)]
-	[String]$Discovery
+	[String]$DiscoveryDisplayName,
+	[Parameter(Mandatory = $false,
+			   Position = 3)]
+	[String]$DiscoveryName,
+	[Parameter(Mandatory = $false,
+			   Position = 4)]
+	[String]$DiscoveryId,
+	[Parameter(Mandatory = $false,
+			   Position = 5)]
+	[Switch]$Wait
 )
 function Start-SCOMDiscovery
 {
@@ -17,7 +26,16 @@ function Start-SCOMDiscovery
 		[String]$ManagementServer,
 		[Parameter(Mandatory = $false,
 				   Position = 2)]
-		[String]$Discovery
+		[String]$DiscoveryDisplayName,
+		[Parameter(Mandatory = $false,
+				   Position = 3)]
+		[String]$DiscoveryName,
+		[Parameter(Mandatory = $false,
+				   Position = 4)]
+		[String]$DiscoveryId,
+		[Parameter(Mandatory = $false,
+				   Position = 5)]
+		[Switch]$Wait
 	)
 	try
 	{
@@ -25,13 +43,38 @@ function Start-SCOMDiscovery
 		{
 			$ManagementServer = $env:COMPUTERNAME
 		}
-		if (!$Discovery)
+<#
+		if ((!$DiscoveryDisplayName) -or (!$DiscoveryName) -or (!$DiscoveryId))
 		{
 			return Write-Host "Missing the Display Name of the Discovery. (ex. Azure SQL*). Run this script like this:`n.\Start-SCOMDiscovery.ps1 -Discovery 'Azure SQL*'" -ForegroundColor Red
 		}
+#>
+		$requestParams = @{ header = 'value' }
+		
+		$PSBoundParameters.GetEnumerator() | ForEach-Object {
+			$value = $_.Value
+			if ($value -is [switch])
+			{
+				$value = $value.IsPresent
+			}
+			
+			$requestParams[$_.Key] = $value
+		}
 		Import-Module OperationsManager
+		'Gathering Discoveries' | Write-Host
 		$Task = Get-SCOMTask -Name Microsoft.SystemCenter.TriggerOnDemandDiscovery
-		$Discoveries = Get-SCOMDiscovery -DisplayName $Discovery
+		if ($DiscoveryDisplayName)
+		{
+			$Discoveries = Get-SCOMDiscovery -DisplayName $DiscoveryDisplayName
+		}
+		elseif ($DiscoveryName)
+		{
+			$Discoveries = Get-SCOMDiscovery -Name $DiscoveryName
+		}
+		elseif ($DiscoveryId)
+		{
+			$Discoveries = Get-SCOMDiscovery -Id $DiscoveryId
+		}
 		'Starting Discoveries (Count: ' + $Discoveries.Count + ')' | Write-Host
 		$i = 0
 		foreach ($Discov in $Discoveries)
@@ -42,25 +85,64 @@ function Start-SCOMDiscovery
 			$output = @()
 			$Override = @{ DiscoveryId = $Discov.Id.ToString(); TargetInstanceId = $Discov.Target.Id.ToString() }
 			$Instance = Get-SCOMClass -Name Microsoft.SystemCenter.ManagementServer | Get-SCOMClassInstance | where { $_.Displayname -like "$ManagementServer`*" }
-			$output += (Start-SCOMTask -Task $Task -Instance $Instance -Override $Override | Select-Object Status, @{ Name = "Discovery"; Expression = { $Discov } }, TimeStarted, TimeScheduled, TimeFinished, Output) | Out-String -Width 4096
+			$CurrentTaskOutput = (Start-SCOMTask -Task $Task -Instance $Instance -Override $Override | Select-Object Status, @{ Name = "Discovery Display Name"; Expression = { $Discov.DisplayName } }, @{ Name = "Discovery Name"; Expression = { $Discov.Name } }, @{ Name = "Discovery Guid"; Expression = { $Discov.Id } }, @{ Name = "Guid"; Expression = { $_.Id } }, TimeScheduled, TimeStarted, TimeFinished, Output)
+			
+            <#
+TaskId               : ff34dc4f-2db3-1736-d9f2-6d85b539ff96
+BatchId              : 3a33f6f7-d2df-4fcb-9fed-736b92230d6a
+SubmittedBy          : Contoso\Administrator
+RunningAs            : 
+TargetObjectId       : 67283979-caa5-86df-e8d1-bfb5876502dc
+TargetClassId        : ab4c891f-3359-3fb6-0704-075fbfe36710
+LocationId           : 67283979-caa5-86df-e8d1-bfb5876502dc
+Status               : Started
+Output               : 
+ErrorCode            : 
+ErrorMessage         : 
+TimeScheduled        : 1/2/2021 5:18:34 AM
+TimeStarted          : 1/2/2021 5:18:35 AM
+TimeFinished         : 
+LastModified         : 1/2/2021 5:18:35 AM
+ProgressValue        : 
+ProgressMessage      : 
+ProgressData         : 
+ProgressLastModified : 
+StatusLastModified   : 1/2/2021 5:18:35 AM
+Id                   : 9f49609c-2152-43ae-acde-97f1c9f9e4e0
+ManagementGroup      : ManagementGroup1
+ManagementGroupId    : e37e57e1-7d7b-79cc-6cdf-95cb3750eaaf
+
+#>
+			$output += $CurrentTaskOutput | Out-String -Width 4096
 			$output
-			$randomnumber = Get-Random -Minimum 3 -Maximum 10
+			$taskresult = $null
+			$randomnumber = Get-Random -Minimum 1 -Maximum 4
+			if ($Wait)
+			{
+				do { $taskResult = Get-SCOMTaskResult -Id $CurrentTaskOutput.Guid | Select-Object Status, @{ Name = "Discovery Display Name"; Expression = { $Discov.DisplayName } }, @{ Name = "Discovery Name"; Expression = { $Discov.Name } }, TimeFinished, Output; Sleep $randomnumber }
+				until (($taskResult.Status -eq 'Succeeded' -or 'Failed') -and ($taskResult.Status -ne 'Started'))
+				$taskResult
+				$Timediff = New-TimeSpan -Start $CurrentTaskOutput.TimeStarted -End $taskResult.TimeFinished
+				
+				$Discov.DisplayName + ' took ' + $Timediff.Seconds + ' seconds.' | Write-Host -ForegroundColor Yellow
+				Write-Host ' '
+			}
 			Start-Sleep -Seconds $randomnumber
 		}
 	}
 	catch
 	{
-		Write-Warning $_
+		Write-Host $_.Exception
 		Write-Host "Unable to trigger the discovery" -ForegroundColor Red
 	}
 }
-if ($ManagementServer -or $Discovery)
+if ($ManagementServer -or $DiscoveryDisplayName -or $DiscoveryName -or $Wait -or $DiscoveryId)
 {
-	Start-SCOMDiscovery -ManagementServer $ManagementServer -Discovery $Discovery
+	Start-SCOMDiscovery -ManagementServer $ManagementServer -DiscoveryDisplayName $DiscoveryDisplayName -DiscoveryName $DiscoveryName -DiscoveryId $DiscoveryId -Wait:$Wait
 }
 else
 {
 	#Enter the Discovery you want to run here.
 	# ex. Start-SCOMDiscovery -Discovery 'Azure SQL*'
-	Start-SCOMDiscovery 
+	Start-SCOMDiscovery
 }
