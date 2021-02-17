5edb1703d0aba56c90f1def05e19b774aeffe110
Blake Drumm <bdrummtelco@gmail.com>
2021-02-17 00:20:33 -0500

    Update Start-SCOMDiscovery.ps1

--- Powershell/Start-SCOMDiscovery.ps1
+++ Powershell/Start-SCOMDiscovery.ps1
@@ -1,7 +1,11 @@
-# Examples
-#  .\Start-SCOMDiscovery.ps1 -DisplayName *Windows*, *Warehouse* -Wait
-#  .\Start-SCOMDiscovery.ps1 -Id d7a25f74-a82b-7977-3a8b-19ae527c86fc -Wait
-#  .\Start-SCOMDiscovery.ps1 -Name Microsoft.SQLServer.Windows.Discovery.DBFilegroup -Output $ENV:USERPROFILE`\Desktop\Output.txt -Wait
+<#
+.AUTHOR
+        Blake Drumm (v-bldrum@microsoft.com)
+ Examples
+  .\Start-SCOMDiscovery.ps1 -DisplayName *Windows*, *Warehouse* -Wait
+  .\Start-SCOMDiscovery.ps1 -Id d7a25f74-a82b-7977-3a8b-19ae527c86fc -Wait
+  .\Start-SCOMDiscovery.ps1 -Name Microsoft.SQLServer.Windows.Discovery.DBFilegroup -Output $ENV:USERPROFILE`\Desktop\Output.txt -Wait
+#>
 param
 (
 	[Parameter(Mandatory = $false,
