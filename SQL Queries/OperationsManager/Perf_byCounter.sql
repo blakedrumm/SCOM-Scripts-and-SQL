b64a05ed0bffeeb785d9ee61652c8f55084dcde6
Blake Drumm <bdrummtelco@gmail.com>
2021-05-11 11:16:10 -0400

    Update Perf_byCounter.sql

--- SQL Queries/OperationsManager/Perf_byCounter.sql
+++ SQL Queries/OperationsManager/Perf_byCounter.sql
@@ -1,9 +1,8 @@
 /*Top 30 performance insertions by perf object and counter name: */
-SELECT        TOP (30) pcv.ObjectName, r.DisplayName AS 'RuleName', pcv.CounterName, MPV.DisplayName AS 'MPDisplayName', pcv.RuleId, COUNT(pcv.CounterName) AS Total
+SELECT        TOP (30) pcv.ObjectName, r.DisplayName AS 'RuleName', pcv.CounterName, pcv.RuleId, COUNT(pcv.CounterName) AS Total
 FROM            PerformanceDataAllView AS pdv INNER JOIN
                          PerformanceCounterView AS pcv ON pdv.PerformanceSourceInternalId = pcv.PerformanceSourceInternalId INNER JOIN
                          RuleView AS r ON pcv.RuleId = r.Id INNER JOIN
-                         ManagementPackView AS MPV ON r.ManagementPackId = MPV.Id
 
-GROUP BY pcv.ObjectName, pcv.CounterName, MPV.DisplayName, pcv.RuleId, r.DisplayName
+GROUP BY pcv.ObjectName, pcv.CounterName, pcv.RuleId, r.DisplayName
 ORDER BY Total DESC
