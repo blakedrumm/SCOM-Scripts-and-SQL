27ff0d8e04be588515ce1212693e7c2322149627
Blake Drumm <bdrummtelco@gmail.com>
2021-06-25 19:31:22 -0400

    Update Install-ManualDCAgents.ps1

--- Powershell/Install-ManualDCAgents.ps1
+++ Powershell/Install-ManualDCAgents.ps1
@@ -14,13 +14,15 @@ Write-Host @"
 Updating SCOM Agents
 ====================
 "@
+# The Domain Controller you want to Install an Agent on
 $AllComputers = 'DC01.contoso.com'
 ForEach ($Computer in $AllComputers)
 {
 	Time-Stamp
 	Write-Host "  $Computer" -ForegroundColor DarkCyan
-	##$Computer
+    #Copy Agent Installer from C:\Temp to C:\Temp on remote computer
 	Copy-Item "C:\Temp\MOMAgent.msi" "\\$Computer\C`$\temp\" -Force -ErrorAction Stop
+    #Copy Agent Update Rollup from C:\Temp to C:\Temp on remote computer
 	Copy-Item "C:\Temp\KB4580254-amd64-Agent.msp" "\\$Computer\C`$\temp\" -Force -ErrorAction Stop
 	Invoke-Command -ComputerName $Computer -ScriptBlock {
 		Stop-process -Name msiexec -Force
@@ -41,12 +43,13 @@ ForEach ($Computer in $AllComputers)
 		Time-Stamp
 		Write-Host "`t - Installing MOMAgent.msi" -ForegroundColor Cyan
 		start-process -FilePath "msiexec.exe" -ArgumentList $args -Wait -ErrorAction Stop
-		
 		sleep 1
+        # Adding 'NT Authority\System' via HSLockdown
 		Time-Stamp
-		Write-Host "`t - Adding 'NT Authority\System' to HSLockdown" -ForegroundColor Cyan
+		Write-Host "`t - Adding 'NT Authority\System' via HSLockdown" -ForegroundColor Cyan
 		start-process -FilePath "C:\Program Files\Microsoft Monitoring Agent\Agent\HSLockdown.exe" -ArgumentList "/A `"NT Authority\System`"" -Wait -ErrorAction Stop
 		sleep 1
+        # Restarting Microsoft Monitoring Agent
 		Time-Stamp
 		Write-Host "`t - Restarting Microsoft Monitoring Agent" -ForegroundColor Cyan
 		Restart-Service HealthService -Force
