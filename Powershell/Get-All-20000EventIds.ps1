b13916c5942222b64e433b557f6fbbc26feceb25
Blake Drumm <bdrummtelco@gmail.com>
2021-06-28 13:17:15 -0400

    Update Get-All-20000EventIds.ps1

--- Powershell/Get-All-20000EventIds.ps1
+++ Powershell/Get-All-20000EventIds.ps1
@@ -1,3 +1,3 @@
 $events = (Get-EventLog -LogName 'Operations Manager' -Source 'OpsMgr Connector' | Where-Object {$_.EventID -eq 20000})
 $message = $events | %{ ($_|Select-Object -Property Message -ExpandProperty Message) }
-$message | %{ ($_ -split "Requesting Device Name : ")[1]} | Select-Object -Unique
+$message | %{ ($_ -split "Requesting Device Name : ")[1]} | Select-Object -Unique | Sort-Object
