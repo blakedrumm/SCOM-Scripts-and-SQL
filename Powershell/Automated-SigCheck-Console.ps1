de386d27ff1fca923381cac952ba43d3bd2051c0
Blake Drumm <bdrummtelco@gmail.com>
2021-08-18 13:29:43 -0400

    Update Automated-SigCheck-Console.ps1

--- Powershell/Automated-SigCheck-Console.ps1
+++ Powershell/Automated-SigCheck-Console.ps1
@@ -3,4 +3,4 @@ $sigcheckPath = 'C:\Users\Administrator.contoso\Downloads\sigcheck.exe'
 #dont edit below
 $installdirectory = ((Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\System Center Operations Manager\12\Setup\Console').InstallDirectory.Split('\') | Select-Object -SkipLast 1) -join ('\')
 $sigpathsplit = $(($sigcheckPath.Split('\') | Select -SkipLast 1) -join '\')
-& $sigcheckPath -c -w $sigpathsplit\ConsoleFile.csv $installdirectory
+& $sigcheckPath -c -w $sigpathsplit\ConsoleFiles.csv $installdirectory
