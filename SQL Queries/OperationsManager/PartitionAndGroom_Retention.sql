afbebd0e8f257f9cc052cd5c14090a2afde8ed8a
Blake Drumm <blakedrumm@microsoft.com>
2021-09-15 13:53:16 -0400

    Remove: WHERE ObjectId NOT IN ('18','17','16','15','10','11')

--- SQL Queries/OperationsManager/PartitionAndGroom_Retention.sql
+++ SQL Queries/OperationsManager/PartitionAndGroom_Retention.sql
@@ -7,5 +7,4 @@ SELECT [ObjectName]
       ,[DataGroomedMaxTime]
       ,[IsInternal]
   FROM [PartitionAndGroomingSettings] WITH (NOLOCK)
-  WHERE ObjectId NOT IN ('18','17','16','15','10','11')
   ORDER BY ObjectName
