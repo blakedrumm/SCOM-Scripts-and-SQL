8b7758376a92a4d2efd074e52331c1d1692a2998
Blake Drumm <blakedrumm@microsoft.com>
2021-10-14 06:42:07 -0400

    Added ability to gather PendingActionType. Thanks Lorne!

--- SQL Queries/OperationsManager/HealthService_Pending.sql
+++ SQL Queries/OperationsManager/HealthService_Pending.sql
@@ -1,1 +1,15 @@
-SELECT * FROM AgentPendingAction WITH (NOLOCK)
+SELECT 
+AgentPendingActionId, 
+AgentName,
+ManagementServerName,
+Case PendingActionType
+When 0 then 'Manual Approval'
+When 1 then 'Push Install'
+When 2 then 'Updated Needed'
+When 10 then 'Repair Failed'
+When 17 then 'Push Install Failed'
+When 18 then 'Update Failed'
+End as [PendingActionType],
+PendingActionData,
+LastModified
+FROM AgentPendingAction WITH (NOLOCK)
