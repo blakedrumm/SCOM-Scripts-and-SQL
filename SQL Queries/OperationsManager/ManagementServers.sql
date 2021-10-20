55cbabc0d86b9709847fbb9f5322406b31596674
Blake Drumm <blakedrumm@microsoft.com>
2021-10-19 22:36:12 -0400

    Use CASE instead of REPLACE()

--- SQL Queries/OperationsManager/ManagementServers.sql
+++ SQL Queries/OperationsManager/ManagementServers.sql
@@ -1,4 +1,17 @@
-SELECT *,REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REASONCODE,17,'The Health Service windows service is paused.'),25,'The Health Service Action Account is misconfigured or has invalid credentials.'),41,'The Health Service failed to parse the new configuration.'),42,'The Health Service failed to load the new configuration.'),43,'A System Rule failed to load.'),49,'Collection of Object State Change Events is stalled.'),50,'Collection of Monitor State Change Events is stalled.'),51,'Collection of Alerts is stalled.'),97,'The Health Service is unable to register with the Event Log Service. The Health Service cannot log additional Heartbeat and Connector events.'),98,'The Health Service is unable to parse configuration XML.'),1,'Reason Unknown. POSSIBLY due to Health Service not heartbeating within 3 minutes to SDK?') AS ReasonCodeResult
+SELECT *,
+Case ReasonCode
+When 17 then 'The Health Service windows service is paused.'
+When 25 then 'The Health Service Action Account is misconfigured or has invalid credentials.'
+When 41 then 'The Health Service failed to parse the new configuration.'
+When 42 then 'The Health Service failed to load the new configuration.'
+When 43 then 'A System Rule failed to load.'
+When 49 then 'Collection of Object State Change Events is stalled.'
+When 50 then 'Collection of Monitor State Change Events is stalled.'
+When 51 then 'Collection of Alerts is stalled.'
+When 97 then 'The Health Service is unable to register with the Event Log Service. The Health Service cannot log additional Heartbeat and Connector events.'
+When 98 then 'The Health Service is unable to parse configuration XML.'
+When 1 then 'Reason Unknown. POSSIBLY due to Health Service not heartbeating within 3 minutes to SDK?'
+End as [ReasonCodeResult]
 FROM MTV_HealthService mtv
 LEFT JOIN AvailabilityHistory ahist
 ON ahist.BaseManagedEntityId = mtv.BaseManagedEntityId
