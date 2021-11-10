6157063d901c2dfe9318f5ee0ac6b610c704ff33
Blake Drumm <bdrummtelco@gmail.com>
2021-11-10 11:57:47 -0500

    Update ACS_EventIDs.sql

--- SQL Queries/OperationsManagerAC/ACS_EventIDs.sql
+++ SQL Queries/OperationsManagerAC/ACS_EventIDs.sql
@@ -1,5 +1,5 @@
-	Declare @total float
-	Select @total = count(EventId) from AdtServer.dvHeader
-	Select count(EventId),EventId, cast(convert(float,(count(EventId)) / (convert(float,@total)) * 100) as decimal(10,2))
-	From AdtServer.dvHeader
-	Group by EventId Order by count(EventId) desc
+Declare @total float
+Select @total = count(EventId) from AdtServer.dvHeader
+Select count(EventId),EventId, cast(convert(float,(count(EventId)) / (convert(float,@total)) * 100) as decimal(10,2))
+From AdtServer.dvHeader
+Group by EventId Order by count(EventId) desc
