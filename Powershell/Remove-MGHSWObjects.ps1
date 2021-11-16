fb1418cf1e3ecbe63c90473bd2d9ebbdf2d46f70
Blake Drumm <blakedrumm@microsoft.com>
2021-11-16 17:15:28 -0500

    Create Remove-MGHSWObjects.ps1

---
+++ Powershell/Remove-MGHSWObjects.ps1
@@ -0,0 +1,25 @@
+#Author: Rogério Barros
+$MSWatcherClass = get-scomclass -name "Microsoft.SystemCenter.ManagementServerWatcher"
+$MSClass =  get-scomclass -name "Microsoft.SystemCenter.ManagementServer"
+
+
+$watchers = ($MSWatcherClass | Get-SCOMClassInstance).DisplayName
+$MS = ($MSClass | Get-SCOMClassInstance).DisplayName
+
+$OrphanedObjects = @()
+$OrphanedWatchers = (Compare-Object $watchers $ms | ? {$_.SideIndicator -eq "<="}).inputobject
+
+foreach ($ow in $OrphanedWatchers)
+{
+    $OrphanedObjects += get-scomclassinstance -Class $MSWatcherClass | ? {$_.DisplayName -eq $ow}
+}
+
+$MG = Get-SCOMManagementGroup
+$discdata = New-Object Microsoft.EnterpriseManagement.ConnectorFramework.IncrementalDiscoveryData
+
+foreach ($obj in $OrphanedObjects)
+{
+	$discdata.Remove($obj)
+}
+
+$discdata.commit($mg)
