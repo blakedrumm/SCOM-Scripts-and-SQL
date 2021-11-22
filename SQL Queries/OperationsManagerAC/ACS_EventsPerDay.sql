9f01c0bac0097cddd3b84e7522941ef6dfb6ae17
Blake Drumm <bdrummtelco@gmail.com>
2021-11-22 14:33:09 -0500

    Check Last 7 days.

--- SQL Queries/OperationsManagerAC/ACS_EventsPerDay.sql
+++ SQL Queries/OperationsManagerAC/ACS_EventsPerDay.sql
@@ -1,4 +1,11 @@
 -- Change the Where line to the correct date range
-Select CollectionTime, EventId, String06 from Adtserver.dvAll
-Where CollectionTime > '2021-11-11 00:01:00:000' and CollectionTime <2021-11-11 23:59:00:000'
-Order by CollectionTime DESC
+--Select CollectionTime, EventId, String06 from Adtserver.dvAll
+--Where CollectionTime > '2021-11-11 00:01:00:000' and CollectionTime <2021-11-11 23:59:00:000'
+--Order by CollectionTime DESC
+--
+-- Get a count for the last 14 days for the total Event Gathered Per Day
+select convert(varchar(10), collectiontime,10),  count(*)
+from AdtServer.dvall (nolock)
+where CollectionTime > dateadd(day, -14, getutcdate())
+group by convert(varchar(10), collectiontime,10)
+order by convert(varchar(10), collectiontime,10) desc
