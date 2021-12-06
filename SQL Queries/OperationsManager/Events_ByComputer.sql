3ebddb869f2d29822ba928b4f4dd0e27989baa9a
Blake Drumm <blakedrumm@microsoft.com>
2021-12-06 10:32:08 -0500

    Update to include Rules - Thank you Alex K!

--- SQL Queries/OperationsManager/Events_ByComputer.sql
+++ SQL Queries/OperationsManager/Events_ByComputer.sql
@@ -1,4 +1,5 @@
-SELECT top 20 LoggingComputer as ComputerName, COUNT(*) AS TotalEvents, Number as EventID 
-FROM EventallView with (NOLOCK) 
-GROUP BY LoggingComputer, Number 
+SELECT top 20 LoggingComputer as ComputerName, COUNT(*) AS TotalEvents, Number as EventID, Name as RuleName, DisplayName as RuleDisplayName
+FROM EventallView eav with (NOLOCK)
+LEFT JOIN RuleView rv with (NOLOCK) on rv.Id = eav.RuleId
+GROUP BY LoggingComputer, Number, Name, DisplayName
 ORDER BY TotalEvents DESC
