1c401ce5a441453627ab1ca9cd230b9e2df4f913
Blake Drumm <blakedrumm@microsoft.com>
2021-12-30 14:17:47 -0500

    Add columns.

--- SQL Queries/OperationsManager/Alerts_ByOldest.sql
+++ SQL Queries/OperationsManager/Alerts_ByOldest.sql
@@ -1,8 +1,12 @@
-SELECT TOP 20 DATEDIFF(Day,TimeRaised,current_timestamp) AS DaysOpened, RepeatCount, AlertStringName, AlertStringDescription, MonitoringObjectDisplayName, MonitoringRuleId, Name, AlertParams, Case ResolutionState
+SELECT TOP 20 DATEDIFF(Day,TimeRaised,current_timestamp) AS DaysOpened, Case Severity
+When 0 then 'Information'
+When 1 then 'Warning'
+When 2 then 'Critical'
+End as [Severity],RepeatCount, AlertStringName, AlertStringDescription, MonitoringObjectDisplayName, MonitoringRuleId, Name, AlertParams, Case ResolutionState
 When 0 then 'Open'
 End as [ResolutionState]
 FROM Alertview WITH (NOLOCK) 
 WHERE Timeraised is not NULL
 AND ResolutionState = 0
-GROUP BY AlertStringName, RepeatCount, TimeRaised, MonitoringObjectDisplayName, Lastmodified,AlertStringDescription, AlertParams, MonitoringRuleId, Name, ResolutionState
+GROUP BY AlertStringName, RepeatCount, TimeRaised, Severity, MonitoringObjectDisplayName, Lastmodified,AlertStringDescription, AlertParams, MonitoringRuleId, Name, ResolutionState
 ORDER BY DaysOpened DESC, RepeatCount DESC
