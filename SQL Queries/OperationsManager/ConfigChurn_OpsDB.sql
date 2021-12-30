b5888df400c93b6359ac9112438215bf786daac2
Blake Drumm <blakedrumm@microsoft.com>
2021-12-30 12:30:48 -0500

    Added Management Pack information and changed the tables to views.

--- SQL Queries/OperationsManager/ConfigChurn_OpsDB.sql
+++ SQL Queries/OperationsManager/ConfigChurn_OpsDB.sql
@@ -1,19 +1,36 @@
 --see all of the configuration changes made in the last 24 hours in OpsDb
-SELECT 
-  TOP 100 EntityTypeId, 
-  TypeName, 
-  COUNT(*) as "Number of changes" 
-FROM 
-  dbo.EntityChangeLog E 
-  join ManagedType MT on MT.ManagedTypeId = e.EntityTypeId 
-WHERE 
-  e.LastModified < GETUTCDATE() -1 
-GROUP BY 
+SELECT
+  TOP 100
+  MTV.Name,
+  MTV.DisplayName,
+  COUNT(*) as "Number of changes",
+  MPV.FriendlyName AS ManagementPackFriendlyName,
+  MPV.Name AS ManagementPackName,
+  MTV.LanguageCode,
+  Case MTV.Hosted
+    When 0 then 'False'
+    When 1 then 'True'
+  End as [IsHosted],
+  EntityTypeId
+FROM
+  dbo.EntityChangeLogView E 
+  join ManagedTypeView MTV on MTV.Id = e.EntityTypeId 
+  left join ManagementPackView MPV on MPV.Id = MTV.ManagementPackId and MPV.LanguageCode = MTV.LanguageCode
+WHERE
+  e.LastModified < GETUTCDATE() -1
+  AND MTV.LanguageCode = (select LanguageCode from __MOMManagementGroupInfo__)
+GROUP BY
   E.EntityTypeId, 
-  TypeName 
-HAVING 
+  MTV.Name,
+  MTV.Hosted,
+  MTV.LanguageCode,
+  MTV.DisplayName,
+  MPV.Name,
+  MPV.FriendlyName,
+  MTV.ManagementPackId
+HAVING
   (
     COUNT(*)
-  ) > 10 
-order by 
-  [Number of changes] desc
+  ) > 10
+order by
+  [Number of changes] DESC, LanguageCode DESC
