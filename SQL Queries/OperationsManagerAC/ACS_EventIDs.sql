20835c7755c7e804472925f3aeee959d2e7bdf1a
Blake Drumm <bdrummtelco@gmail.com>
2021-12-30 18:56:43 -0500

    Update ACS_EventIDs.sql

--- SQL Queries/OperationsManagerAC/ACS_EventIDs.sql
+++ SQL Queries/OperationsManagerAC/ACS_EventIDs.sql
@@ -1,5 +1,5 @@
 Declare @total float
 Select @total = count(EventId) from AdtServer.dvHeader
-Select count(EventId),EventId, cast(convert(float,(count(EventId)) / (convert(float,@total)) * 100) as decimal(10,2))
+Select count(EventId) AS Count,EventId, cast(convert(float,(count(EventId)) / (convert(float,@total)) * 100) as decimal(10,2)) AS PercentOfTotalEvents
 From AdtServer.dvHeader
 Group by EventId Order by count(EventId) desc
