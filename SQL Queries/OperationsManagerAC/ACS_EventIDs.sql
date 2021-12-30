c34d19ae5d70cc69c53926edb87e0223a1158f0b
Blake Drumm <bdrummtelco@gmail.com>
2021-12-30 18:57:59 -0500

    Update ACS_EventIDs.sql

--- SQL Queries/OperationsManagerAC/ACS_EventIDs.sql
+++ SQL Queries/OperationsManagerAC/ACS_EventIDs.sql
@@ -1,3 +1,4 @@
+-- Get a count of the total ACS Events Collected, and how much percent the Event is taking up the ACS DB
 Declare @total float
 Select @total = count(EventId) from AdtServer.dvHeader
 Select count(EventId) AS Count,EventId, cast(convert(float,(count(EventId)) / (convert(float,@total)) * 100) as decimal(10,2)) AS PercentOfTotalEvents
