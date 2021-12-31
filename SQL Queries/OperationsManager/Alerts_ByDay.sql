c3336b45f826e7c6f48fb6cd5ed3fd12297046a0
Blake Drumm <blakedrumm@microsoft.com>
2021-12-30 19:08:01 -0500

    Update Alerts_ByDay.sql

--- SQL Queries/OperationsManager/Alerts_ByDay.sql
+++ SQL Queries/OperationsManager/Alerts_ByDay.sql
@@ -1,5 +1,5 @@
-﻿SELECT CONVERT(VARCHAR(20), TimeAdded, 102) AS DayAdded, COUNT(*) AS NumAlertsPerDay 
-FROM Alert WITH (NOLOCK) 
+SELECT CONVERT(VARCHAR(20), TimeAdded, 102) AS DayAdded, COUNT(*) AS NumAlertsPerDay 
+FROM AlertView
 WHERE TimeRaised is not NULL 
 GROUP BY CONVERT(VARCHAR(20), TimeAdded, 102) 
 ORDER BY DayAdded DESC
