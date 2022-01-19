cad9315c0e962336d914832cc354d9a868d15b27
Blake Drumm <bdrummtelco@gmail.com>
2022-01-19 03:19:03 -0500

    Update Remove_SCOM_Agent.ps1

--- Powershell/Remove SCOM Agent/Remove_SCOM_Agent.ps1
+++ Powershell/Remove SCOM Agent/Remove_SCOM_Agent.ps1
@@ -6,7 +6,7 @@
         If there are issues in removing the SCOM Agent, this script may be able to help forcibly remove the application from the server.
         Commands and references used in the script are taken from a SCOM 2019 agent installation and may be different in other versions.
 
-        This is not 100% foolproof nor does it guarantee complete removal of all components, the intent is to remove as many _obvious_ references
+        This is not 100% foolproof nor does it guarantee complete removal of all components, the intent is to remove as many -obvious- references
         as possible so that the machine is no longer recognized to have an agent installed, or had one previously.
 
         This script will attempt to remove all registered services, performance counters, DLLs, and program files. This script makes direct deletions
