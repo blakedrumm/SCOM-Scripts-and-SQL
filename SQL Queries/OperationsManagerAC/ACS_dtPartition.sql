a10ac47034c86c2ef653ac0bf12532a437561fc2
Blake Drumm <bdrummtelco@gmail.com>
2022-01-31 10:28:26 -0500

    Update ACS_dtPartition.sql

--- SQL Queries/OperationsManagerAC/ACS_dtPartition.sql
+++ SQL Queries/OperationsManagerAC/ACS_dtPartition.sql
@@ -1,1 +1,7 @@
+-- Status:
+-- 0: active, set by collector
+-- 1: inactive, mark for closing during collector startup & indexing, set manually
+-- 2: archived, ready for deletion, set from outside the collector
+-- 100 - 108: closed, indexing in progress
+-- 109: indexing complete
 select * from dtpartition order by partitionstarttime DESC
