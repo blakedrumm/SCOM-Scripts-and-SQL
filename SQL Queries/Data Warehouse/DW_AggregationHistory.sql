7a78e767212ddbee7a3ee5d1a3ef0e6637d485db
Blake Drumm <bdrummtelco@gmail.com>
2022-02-15 17:04:34 -0500

    Add ability to gather the aggregation type name

--- SQL Queries/Data Warehouse/DW_AggregationHistory.sql
+++ SQL Queries/Data Warehouse/DW_AggregationHistory.sql
@@ -1,13 +1,14 @@
 SELECT ds.DatasetDefaultName,
-  sdah.AggregationDateTime,
-  sdah.AggregationTypeId,
-  sdah.FirstAggregationStartDateTime,
-  sdah.FirstAggregationDurationSeconds,
-  sdah.LastAggregationStartDateTime,
-  sdah.LastAggregationDurationSeconds,
-  sdah.DirtyInd,
-  sdah.DataLastReceivedDateTime,
-  sdah.AggregationCount
+       sdah.AggregationDateTime,
+       atype.AggregationTypeDefaultName,
+       sdah.AggregationCount,
+       sdah.DirtyInd,
+       sdah.FirstAggregationStartDateTime,
+       sdah.FirstAggregationDurationSeconds,
+       sdah.LastAggregationStartDateTime,
+       sdah.LastAggregationDurationSeconds,
+       sdah.DataLastReceivedDateTime
 FROM StandardDatasetAggregationHistory sdah WITH(NOLOCK)
 JOIN Dataset ds WITH (NOLOCK) ON sdah.DatasetId = ds.DatasetId
+JOIN AggregationType atype WITH (NOLOCK) ON sdah.AggregationTypeId = atype.AggregationTypeId
 ORDER BY StandardDatasetAggregationHistoryRowId DESC
