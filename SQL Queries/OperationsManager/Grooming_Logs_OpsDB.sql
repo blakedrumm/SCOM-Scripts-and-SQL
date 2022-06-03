6c59750bb3482a2bbc2400b75a8a611606d241fb
Blake Drumm <blakedrumm@microsoft.com>
2022-06-03 18:01:24 -0400

    Update Grooming_Logs_OpsDB.sql

--- SQL Queries/OperationsManager/Grooming_Logs_OpsDB.sql
+++ SQL Queries/OperationsManager/Grooming_Logs_OpsDB.sql
@@ -2,6 +2,7 @@ select InternalJobHistoryId,
 Command,
 REPLACE(REPLACE(StatusCode,0,'Failed'),1,'Successful') as 'Status',
 TimeStarted,
-TimeFinished
+TimeFinished,
+CONCAT(DATEDIFF (second,TimeStarted,TimeFinished),' seconds') AS 'DurationSeconds'
 from InternalJobHistory WITH (NOLOCK)
 order by InternalJobHistoryId DESC
