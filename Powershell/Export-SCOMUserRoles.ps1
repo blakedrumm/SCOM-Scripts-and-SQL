980056a9ba73c2a395c504cad84c538bc1d54e8f
Blake Drumm <blakedrumm@microsoft.com>
2022-06-20 14:53:25 -0400

    Create Export-SCOMUserRoles.ps1

---
+++ Powershell/Export-SCOMUserRoles.ps1
@@ -0,0 +1,21 @@
+# This script will export the SCOM User Roles to CSV or Text File Format.
+# -----------------------------------------------
+# Outputs the file to the current users desktop
+# Initial Upload: July 20th, 2022
+# -----------------------------------------------
+$UserRoles = @()
+$UserRoleList = Get-SCOMUserRole
+Write-Host "  Processing User Role:  " -ForegroundColor Cyan
+foreach ($UserRole in $UserRoleList)
+{
+  Write-Host "    $UserRole" -ForegroundColor Magenta
+  $UserRoles += New-Object -TypeName psobject -Property @{
+    Name = $UserRole.Name;
+    DisplayName = $UserRole.DisplayName;
+    Description = $UserRole.Description;
+    Users = ($UserRole.Users -join "; ");
+  }
+}
+$UserRolesOutput = $UserRoles | Select-Object Name, DisplayName, Description, Users
+$UserRolesOutput | Out-File "$env:USERPROFILE`\Desktop\UserRoles.txt" -Width 4096
+$UserRolesOutput | Export-CSV -Path "$env:USERPROFILE`\Desktop\UserRoles.csv" -NoTypeInformation
