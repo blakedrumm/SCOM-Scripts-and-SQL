dfa720624d62beafbfb0e1317e776a11e45bc804
Blake Drumm <blakedrumm@microsoft.com>
2022-09-30 16:22:11 -0400

    Update Check-AgentFailover.ps1

--- Powershell/Agents Failover/Check-AgentFailover.ps1
+++ Powershell/Agents Failover/Check-AgentFailover.ps1
@@ -1,21 +1,29 @@
-Param ([string]$parameter)
-
+# ===============================
 # Author: Blake Drumm (blakedrumm@microsoft.com)
+# Script location: https://github.com/blakedrumm/SCOM-Scripts-and-SQL/blob/master/Powershell/Agents%20Failover/Check-AgentFailover.ps1
+# ===============================
 
+# ===============================
+#region Script Start
 Import-Module OperationsManager;
+$agents = Get-SCOMAgent
+#endregion ScriptStart
+# ===============================
 
-$ErrorActionPreference = "silentlycontinue";
-$output = [pscustomobject]@{ }
-
-If (!($parameter -eq "")) { $agents = Get-SCOMAgent $parameter }
-else { $agents = Get-SCOMAgent };
-
+# ===============================
+#region Initiate Variables
+$primaryOutput = @()
 $i = 0
+#endregion Initiate Variables
+# ===============================
+
+#region Main Script
 ForEach ($agent in $agents)
 {
 	$i++
 	$i = $i
 	Write-Output "($i / $($agents.count)) $($agent.DisplayName)"
+	$output = [pscustomobject]@{ }
 	$output | Add-Member -MemberType NoteProperty -Name 'Server Agent' -Value $agent.DisplayName -ErrorAction SilentlyContinue
 	
 	If (($agent.GetPrimaryManagementServer()).IsGateway -eq $true)
@@ -29,10 +37,13 @@ ForEach ($agent in $agents)
 		$output | Add-Member -MemberType NoteProperty -Name 'Primary Management Server [Gateway]' -Value $null -ErrorAction SilentlyContinue
 	};
 	
-	$failover = $agent.GetFailoverManagementServers();
-	
-	$output | Add-Member -MemberType NoteProperty -Name 'Failover Management Server' -Value $failover -ErrorAction SilentlyContinue
+	$output | Add-Member -MemberType NoteProperty -Name 'Failover Management Server' -Value $(($agent.GetFailoverManagementServers()).DisplayName -join ", ") -ErrorAction SilentlyContinue
+	$primaryOutput += $output
 	
 };
-$output | ft *
-$output | Export-Csv -Path C:\Temp\AgentList_of_Primary_and_Failover.csv -NoTypeInformation
+#endregion Main Script
+
+#region Output
+$primaryOutput | ft *
+$primaryOutput | Export-Csv -Path C:\Temp\AgentList_of_Primary_and_Failover.csv -NoTypeInformation
+#endregion Output
