377a57b7fab806efca32b20129e31243877952dd
Blake Drumm <blakedrumm@microsoft.com>
2022-10-07 17:38:12 -0400

    Remove function

--- Powershell/Export-ManagementPacks.ps1
+++ Powershell/Export-ManagementPacks.ps1
@@ -1,53 +1,51 @@
-Function MP-Export
+$OutputPath = 'C:\Temp'
+
+if ((Test-Path -Path "$OutputPath\Unsealed Management Packs") -eq $false)
 {
-	$OutputPath = 'C:\Temp'
-	if ((Test-Path -Path "$OutputPath\Unsealed Management Packs") -eq $false)
-	{
-		Write-Host "  Creating Folder: $OutputPath\Unsealed Management Packs" -ForegroundColor Gray
-		md "$OutputPath\Unsealed Management Packs" | Out-Null
-	}
-	else
-	{
-		Write-Host "  Existing Folder Found: $OutputPath\Unsealed Management Packs" -ForegroundColor Gray
-		Remove-Item "$OutputPath\Unsealed Management Packs" -Recurse | Out-Null
-		Write-Host "   Deleting folder contents" -ForegroundColor Gray
-		md "$OutputPath\Unsealed Management Packs" | out-null
-		Write-Host "    Folder Created: $OutputPath\Unsealed Management Packs" -ForegroundColor Gray
-	}
-	
-	try
-	{
-		Get-SCOMManagementPack | Where{ $_.Sealed -eq $false } | Export-SCOMManagementPack -path "$OutputPath\Unsealed Management Packs" | out-null
-		Write-Host "    Completed Exporting Management Packs" -ForegroundColor Green
-	}
-	catch
-	{
-		Add-Content -Path "$OutputPath\Unsealed Management Packs\Error.txt" -Value $Error
-		Write-Warning $_
-	}
-	
-	if ((Test-Path -Path "$OutputPath\Sealed Management Packs") -eq $false)
-	{
-		Write-Host "  Creating Folder: $OutputPath\Sealed Management Packs" -ForegroundColor Gray
-		md "$OutputPath\Sealed Management Packs" | Out-Null
-	}
-	else
-	{
-		Write-Host "  Existing Folder Found: $OutputPath\Sealed Management Packs" -ForegroundColor Gray
-		Remove-Item "$OutputPath\Sealed Management Packs" -Recurse | Out-Null
-		Write-Host "   Deleting folder contents" -ForegroundColor Gray
-		md "$OutputPath\Sealed Management Packs" | out-null
-		Write-Host "    Folder Created: $OutputPath\Sealed Management Packs" -ForegroundColor Gray
-	}
-	
-	try
-	{
-		Get-SCOMManagementPack | Where{ $_.Sealed -eq $true } | Export-SCOMManagementPack -path "$OutputPath\Sealed Management Packs" | out-null
-		Write-Host "    Completed Exporting Management Packs" -ForegroundColor Green
-	}
-	catch
-	{
-		Add-Content -Path "$OutputPath\Sealed Management Packs\Error.txt" -Value $Error
-		Write-Warning $_
-	}
+	Write-Host "  Creating Folder: $OutputPath\Unsealed Management Packs" -ForegroundColor Gray
+	md "$OutputPath\Unsealed Management Packs" | Out-Null
+}
+else
+{
+	Write-Host "  Existing Folder Found: $OutputPath\Unsealed Management Packs" -ForegroundColor Gray
+	Remove-Item "$OutputPath\Unsealed Management Packs" -Recurse | Out-Null
+	Write-Host "   Deleting folder contents" -ForegroundColor Gray
+	md "$OutputPath\Unsealed Management Packs" | out-null
+	Write-Host "    Folder Created: $OutputPath\Unsealed Management Packs" -ForegroundColor Gray
+}
+
+try
+{
+	Get-SCOMManagementPack | Where{ $_.Sealed -eq $false } | Export-SCOMManagementPack -path "$OutputPath\Unsealed Management Packs" | out-null
+	Write-Host "    Completed Exporting Management Packs" -ForegroundColor Green
+}
+catch
+{
+	Add-Content -Path "$OutputPath\Unsealed Management Packs\Error.txt" -Value $Error
+	Write-Warning $_
+}
+
+if ((Test-Path -Path "$OutputPath\Sealed Management Packs") -eq $false)
+{
+	Write-Host "  Creating Folder: $OutputPath\Sealed Management Packs" -ForegroundColor Gray
+	md "$OutputPath\Sealed Management Packs" | Out-Null
+}
+else
+{
+	Write-Host "  Existing Folder Found: $OutputPath\Sealed Management Packs" -ForegroundColor Gray
+	Remove-Item "$OutputPath\Sealed Management Packs" -Recurse | Out-Null
+	Write-Host "   Deleting folder contents" -ForegroundColor Gray
+	md "$OutputPath\Sealed Management Packs" | out-null
+	Write-Host "    Folder Created: $OutputPath\Sealed Management Packs" -ForegroundColor Gray
+}
+
+try
+{
+	Get-SCOMManagementPack | Where{ $_.Sealed -eq $true } | Export-SCOMManagementPack -path "$OutputPath\Sealed Management Packs" | out-null
+	Write-Host "    Completed Exporting Management Packs" -ForegroundColor Green
+}
+catch
+{
+	Add-Content -Path "$OutputPath\Sealed Management Packs\Error.txt" -Value $Error
+	Write-Warning $_
 }
