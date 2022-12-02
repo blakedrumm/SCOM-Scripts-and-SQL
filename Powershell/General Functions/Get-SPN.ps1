3053e3426e1c55820bc962bbec62d268b2ea6e17
Blake Drumm <blakedrumm@microsoft.com>
2022-12-01 20:09:05 -0500

    Updated script :badger:

--- Powershell/General Functions/Get-SPN.ps1
+++ Powershell/General Functions/Get-SPN.ps1
@@ -1,93 +1,120 @@
-function Get-SPN
-{
-	## Active Directory: PowerShell Function to Get Service Principal Names (SPNs) ##
-	# Checked on 11/23/2021
-	## Resource: https://gallery.technet.microsoft.com/scriptcenter/Get-SPN-Get-Service-3bd5524a
-	
+
+## Active Directory: PowerShell Function to Get Service Principal Names (SPNs) ##
+# Checked on 11/23/2021
+## Resource: https://gallery.technet.microsoft.com/scriptcenter/Get-SPN-Get-Service-3bd5524a
+
 <#
-    .SYNOPSIS
-        This function will retrieve Service Principal Names (SPNs), with filters for computer name, service type, and port/instance
-
-    .DESCRIPTION
-        Get Service Principal Names
-
-        Output includes:
-            ComputerName - SPN Host
-            Specification - SPN Port (or Instance)
-            ServiceClass - SPN Service Class (MSSQLSvc, HTTP, etc.)
-            sAMAccountName - sAMAccountName for the AD object with a matching SPN
-            SPN - Full SPN string
-
-    .PARAMETER ComputerName
-        One or more hostnames to filter on.  Default is *
-
-    .PARAMETER ServiceClass
-        Service class to filter on.
-        
-        Examples:
-            HOST
-            MSSQLSvc
-            TERMSRV
-            RestrictedKrbHost
-            HTTP
-
-    .PARAMETER Specification
-        Filter results to this specific port or instance name
-
-    .PARAMETER SPN
-        If specified, filter explicitly and only on this SPN.  Accepts Wildcards.
-
-    .PARAMETER Domain
-        If specified, search in this domain. Use a fully qualified domain name, e.g. contoso.org
-
-        If not specified, we search the current user's domain
-
-    .EXAMPLE
-        Get-Spn -ServiceClass MSSQLSvc
-        
-        #This command gets all MSSQLSvc SPNs for the current domain
-    
-    .EXAMPLE
-        Get-Spn -ComputerName SQLServer54, SQLServer55
-        
-        #List SPNs associated with SQLServer54, SQLServer55
-    
-    .EXAMPLE
-        Get-SPN -SPN http*
-
-        #List SPNs maching http*
-    
-    .EXAMPLE
-        Get-SPN -ComputerName SQLServer54 -Domain Contoso.org
-
-        # List SPNs associated with SQLServer54 in contoso.org
-
-    .NOTES 
-        Adapted from
-            http://www.itadmintools.com/2011/08/list-spns-in-active-directory-using.html
-            http://poshcode.org/3234
-        Version History 
-            v1.0   - Chad Miller - Initial release 
-            v1.1   - ramblingcookiemonster - added parameters to specify service type, host, and specification
-            v1.1.1 - ramblingcookiemonster - added parameterset for explicit SPN lookup, added ServiceClass to results
-
-    .FUNCTIONALITY
-        Active Directory             
-#>
+	.SYNOPSIS
+		This function will retrieve Service Principal Names (SPNs), with filters for computer name, service type, and port/instance
 	
+	.DESCRIPTION
+		Get Service Principal Names
+		
+		Output includes:
+		ComputerName - SPN Host
+		Specification - SPN Port (or Instance)
+		ServiceClass - SPN Service Class (MSSQLSvc, HTTP, etc.)
+		sAMAccountName - sAMAccountName for the AD object with a matching SPN
+		SPN - Full SPN string
+	
+	.PARAMETER ComputerName
+		One or more hostnames to filter on.  Default is *
+	
+	.PARAMETER ServiceClass
+		Service class to filter on.
+		
+		Examples:
+		HOST
+		MSSQLSvc
+		TERMSRV
+		RestrictedKrbHost
+		HTTP
+	
+	.PARAMETER Specification
+		Filter results to this specific port or instance name.
+	
+	.PARAMETER SPN
+		If specified, filter explicitly and only on this SPN.  Accepts Wildcards.
+	
+	.PARAMETER Domain
+		If specified, search in this domain. Use a fully qualified domain name, e.g. contoso.org
+		
+		If not specified, we search the current user's domain.
+	
+	.EXAMPLE
+		Get-Spn -ServiceClass MSSQLSvc
+		
+		#This command gets all MSSQLSvc SPNs for the current domain
+	
+	.EXAMPLE
+		Get-Spn -ComputerName SQLServer54, SQLServer55
+		
+		#List SPNs associated with SQLServer54, SQLServer55
+	
+	.EXAMPLE
+		Get-SPN -SPN http*
+		
+		#List SPNs matching http*
+	
+	.EXAMPLE
+		Get-SPN -ComputerName SQLServer54 -Domain Contoso.org
+		
+		# List SPNs associated with SQLServer54 in contoso.org
+	
+	.NOTES
+		Adapted from
+		http://www.itadmintools.com/2011/08/list-spns-in-active-directory-using.html
+		http://poshcode.org/3234
+		Version History
+		v1.0   - Chad Miller - Initial release
+		v1.1   - ramblingcookiemonster - added parameters to specify service type, host, and specification
+		v1.1.1 - ramblingcookiemonster - added parameterset for explicit SPN lookup, added ServiceClass to results
+		v1.2   - blakedrumm - Fixed regular expression for determining the computer to return
+	
+	.FUNCTIONALITY
+		Active Directory
+#>
+[CmdletBinding(DefaultParameterSetName = 'Parse')]
+param
+(
+	[Parameter(ParameterSetName = 'Parse',
+			   ValueFromPipeline = $true,
+			   ValueFromPipelineByPropertyName = $true,
+			   Position = 0,
+			   HelpMessage = 'One or more hostnames to filter on.  Default is *')]
+	[string[]]$ComputerName = "*",
+	[Parameter(ParameterSetName = 'Parse',
+			   HelpMessage = 'Service class to filter on.')]
+	[string]$ServiceClass = "*",
+	[Parameter(ParameterSetName = 'Parse',
+			   HelpMessage = 'Filter results to this specific port or instance name.')]
+	[string]$Specification = "*",
+	[Parameter(ParameterSetName = 'Explicit',
+			   HelpMessage = 'If specified, filter explicitly and only on this SPN.  Accepts Wildcards.')]
+	[string]$SPN,
+	[Parameter(HelpMessage = "If specified, search in this domain. Use a fully qualified domain name, e.g. contoso.org. If not specified, we search the current user's domain.")]
+	[string]$Domain
+)
+function Invoke-GetSPN
+{
 	[cmdletbinding(DefaultParameterSetName = 'Parse')]
 	param (
-		[Parameter(Position = 0,
+		[Parameter(ParameterSetName = 'Parse',
 				   ValueFromPipeline = $true,
 				   ValueFromPipelineByPropertyName = $true,
-				   ParameterSetName = 'Parse')]
+				   Position = 0,
+				   HelpMessage = 'One or more hostnames to filter on.  Default is *')]
 		[string[]]$ComputerName = "*",
-		[Parameter(ParameterSetName = 'Parse')]
+		[Parameter(ParameterSetName = 'Parse',
+				   HelpMessage = 'Service class to filter on.')]
 		[string]$ServiceClass = "*",
-		[Parameter(ParameterSetName = 'Parse')]
+		[Parameter(ParameterSetName = 'Parse',
+				   HelpMessage = 'Filter results to this specific port or instance name.')]
 		[string]$Specification = "*",
-		[Parameter(ParameterSetName = 'Explicit')]
+		[Parameter(ParameterSetName = 'Explicit',
+				   HelpMessage = 'If specified, filter explicitly and only on this SPN.  Accepts Wildcards.')]
 		[string]$SPN,
+		[Parameter(HelpMessage = "If specified, search in this domain. Use a fully qualified domain name, e.g. contoso.org. If not specified, we search the current user's domain.")]
 		[string]$Domain
 	)
 	BEGIN
@@ -116,6 +143,7 @@ function Get-SPN
 			#To use same logic as 'parse' parameterset, set these variables up...
 			$ComputerName = @("*")
 			$Specification = "*"
+            $ServiceFilter = $SPN.Replace('/','\/')
 		}
 		
 		#Set up objects for searching
@@ -129,44 +157,59 @@ function Get-SPN
 		#Loop through all the computers and search!
 		foreach ($computer in $ComputerName)
 		{
+			if ($computer -eq '*' -and $SpecificationFilter -eq '*')
+			{
+				$endQuery = '*'
+			}
+			else
+			{
+				$endQuery = "$computer$SpecificationFilter"
+			}
 			#Set filter - Parse SPN or use the explicit SPN parameter
 			if ($PsCmdlet.ParameterSetName -like "Parse")
 			{
-				$filter = "(servicePrincipalName=$ServiceClass/$computer$SpecificationFilter)"
+				$filter = "(servicePrincipalName=$ServiceClass/$endQuery)"
 			}
 			else
 			{
 				$filter = "(servicePrincipalName=$SPN)"
 			}
+			$filter = $filter.Replace("/*/*", "/*")
 			$searcher.Filter = $filter
-			
+			$output = @()
 			Write-Verbose "Searching for SPNs with filter $filter"
 			foreach ($result in $searcher.FindAll())
 			{
-				
 				$account = $result.GetDirectoryEntry()
-				foreach ($servicePrincipalName in $account.servicePrincipalName.Value)
+				foreach ($servicePrincipalName in $account.servicePrincipalName)
 				{
+					Write-Verbose "`t`t$servicePrincipalName"
 					#Regex will capture computername and port/instance
-					if ($servicePrincipalName -match "^(?<ServiceClass>$ServiceFilter)\/(?<computer>[^\.|^:]+)[^:]*(:{1}(?<port>\w+))?$")
+					if ($servicePrincipalName -match "^(?<ServiceClass>$ServiceFilter)(\/)(?<computer>$($Computer.Replace('*', '.*')))[^:]*(:{1}(?<port>\w+))?$")
 					{
-						
 						#Build up an object, get properties in the right order, filter on computername
-						New-Object psobject -property @{
+						$output += New-Object psobject -property @{
 							ComputerName = $matches.computer
 							ServiceClass = $matches.ServiceClass
 							sAMAccountName = $($account.sAMAccountName)
 							distinguishedName = $($account.distinguishedName)
 							whenChanged  = $($account.whenChanged)
 							SPN		     = $servicePrincipalName
-						} |
-						Select-Object ComputerName, ServiceClass, sAMAccountName, distinguishedName, whenChanged, SPN #|
+						} #|
 						#To get results that match parameters, filter on comp and spec
 						#Where-Object { $_.ComputerName -like $computer -and $_.Specification -like $Specification }
 					}
 				}
 			}
+			return $output | Select-Object ComputerName, ServiceClass, sAMAccountName, distinguishedName, whenChanged, SPN | Sort-Object ComputerName, ServiceClass, sAMAccountName, whenChanged
 		}
 	}
 }
-Get-SPN
+if (($ComputerName -ne '*') -or ($ServiceClass -ne '*') -or ($Specification -ne '*') -or $SPN -or $Domain)
+{
+	Invoke-GetSPN -ComputerName $ComputerName -ServiceClass $ServiceClass -Specification $Specification -SPN $SPN -Domain $Domain
+}
+else
+{
+	Invoke-GetSPN
+}
