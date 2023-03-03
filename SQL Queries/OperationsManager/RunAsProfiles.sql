06f2350fa732f67712c57dae0cc14c522ca738a9
Blake Drumm <blakedrumm@microsoft.com>
2023-03-03 14:06:57 -0500

    Added RunAs Profile Display Name - Thank you Alex Kremenetskiy :peace_symbol:

--- SQL Queries/OperationsManager/RunAsProfiles.sql
+++ SQL Queries/OperationsManager/RunAsProfiles.sql
@@ -1,14 +1,15 @@
 --get all Run As profiles and associated accounts, 
 	--credentials, Secure Storage ID values and targets
 SELECT [Profile].SecureReferenceName ProfileName
-	, Account.Name AccountName
-	, Account.Domain
-	, Account.UserName
-	, Account.LastModified
-	, [Override].OverrideName
-	, [Override].Value SSID
-	, bme.FullName
-	, mt.TypeName
+    , lt.LTValue
+    , Account.Name AccountName
+    , Account.Domain
+    , Account.UserName
+    , Account.LastModified
+    , [Override].OverrideName
+    , [Override].Value SSID
+    , bme.FullName
+    , mt.TypeName
 FROM CredentialManagerSecureStorage AS Account WITH (NOLOCK)
 JOIN SecureReferenceOverride AS [Override] WITH (NOLOCK)
 ON CONVERT (varchar(80), Account.SecureStorageId, 2) = [Override].Value
@@ -18,4 +19,5 @@ JOIN ManagedType mt WITH (NOLOCK)
 ON [Override].TypeContext = mt.ManagedTypeId
 LEFT JOIN BaseManagedEntity bme WITH (NOLOCK)
 ON [Override].InstanceContext = bme.BaseManagedEntityId
+join LocalizedText lt on lt.LTStringId = [Profile].SecureReferenceId and lt.LTStringType = 1
 ORDER BY ProfileName
