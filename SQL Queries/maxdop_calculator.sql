2d0e2172db48f17998490f005be388da6b5a54c7
Blake Drumm <blakedrumm@microsoft.com>
2023-05-24 17:29:53 -0400

    Update license information :old_key:

--- SQL Queries/maxdop_calculator.sql
+++ SQL Queries/maxdop_calculator.sql
@@ -1,16 +1,17 @@
 /*
 -- +----------------------------------------------------------------------------+
--- |                  			DBA Mastery		                |
--- |                        dbamastery@outlook.com                              |
--- |                      http://www.dbamastery.com                             |
+-- |                  			Blake Drumm             		                |
+-- |                        blakedrumm@microsoft.com                            |
+-- |                      http://www.blakedrumm.com                             |
+-- |             Original Author: DBA Mastery (http://www.dbamastery.com)       |
 -- |----------------------------------------------------------------------------|
 -- |                                                                            |
 -- |----------------------------------------------------------------------------|
 -- | DATABASE : SQL Server                                                      |
 -- | FILE     : maxdop_calculator.sql                                       	|
 -- | CLASS    : Performance tuning                                              |
--- | PURPOSE  :	Calculates the optimal value for MAXDOP (> SQL 2016 )		|
--- |										|
+-- | PURPOSE  :	Calculates the optimal value for MAXDOP (> SQL 2016 )	    	|
+-- |                                    										|
 -- | NOTE     : As with any code, ensure to test this script in a development   |
 -- |            environment before attempting to run it in production.          |
 -- |                                                                            |
@@ -18,6 +19,29 @@
 -- |            MSDN MaxDOP calculator: https://goo.gl/hzyxY1                   |
 -- +----------------------------------------------------------------------------+
 Copyright (c) 2019 DBA Mastery
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-- +----------------------------------------------------------------------------+
+MIT License
+
+Copyright (c) 2023 Blake Drumm
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
