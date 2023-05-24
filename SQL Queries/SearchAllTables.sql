7e176444b1b0456c23e69f8a570aa3a9f822fdc9
Blake Drumm <blakedrumm@microsoft.com>
2023-05-24 15:12:04 -0400

    Updated query :package:

--- SQL Queries/SearchAllTables.sql
+++ SQL Queries/SearchAllTables.sql
@@ -9,71 +9,56 @@ GO
 --Here is the complete stored procedure code: 
 
 
-CREATE PROC [dbo].[SearchAllTables]
+IF OBJECT_ID('[dbo].[SearchAllTables]', 'P') IS NOT NULL
+    DROP PROCEDURE [dbo].[SearchAllTables]
+GO
+
+-- Create a stored procedure called [dbo].[SearchAllTables]
+CREATE PROCEDURE [dbo].[SearchAllTables]
 (
-                @SearchStr nvarchar(100)
+    @SearchStr NVARCHAR(100) -- Input parameter to search for a specific string
 )
 AS
 BEGIN
+    SET NOCOUNT ON;
 
-                -- Copyright © 2002 Narayana Vyas Kondreddi. All rights reserved.
-                -- Purpose: To search all columns of all tables for a given search string
-                -- Written by: Narayana Vyas Kondreddi
-                -- Site: http://vyaskn.tripod.com
-                -- Tested on: SQL Server 7.0 and SQL Server 2000
-                -- Date modified: 28th July 2002 22:50 GMT
+    -- Create a temporary table to store the search results
+    CREATE TABLE #Results (TableName NVARCHAR(256), ColumnName NVARCHAR(128), ColumnValue NVARCHAR(MAX), RowNumber INT);
 
+    -- Declare variables to store table and column names, and construct the search string
+    DECLARE @TableName NVARCHAR(256), @ColumnName NVARCHAR(128), @SearchStr2 NVARCHAR(110);
+    SET @SearchStr2 = '%' + @SearchStr + '%'; -- Add wildcard characters to search string
 
-                CREATE TABLE #Results (ColumnName nvarchar(370), ColumnValue nvarchar(3630))
+    -- Declare a dynamic SQL variable to build the dynamic SQL statement
+    DECLARE @DynamicSQL NVARCHAR(MAX) = N'';
 
-                SET NOCOUNT ON
+    -- Build the dynamic SQL statement to search for the specified string in all tables and columns
+    SELECT @DynamicSQL += 
+        N'INSERT INTO #Results (TableName, ColumnName, ColumnValue, RowNumber) ' +
+        N'SELECT ''' + QUOTENAME(t.TABLE_SCHEMA) + '.' + QUOTENAME(t.TABLE_NAME) + ''', ' +
+        N'''' + QUOTENAME(c.COLUMN_NAME) + ''', ' +
+        N'CAST(' + QUOTENAME(c.COLUMN_NAME) + ' AS NVARCHAR(MAX)), ' +
+        N'ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) ' +
+        N'FROM ' + QUOTENAME(t.TABLE_SCHEMA) + '.' + QUOTENAME(t.TABLE_NAME) + ' (NOLOCK) ' +
+        N'WHERE ' + QUOTENAME(c.COLUMN_NAME) + ' LIKE @SearchStr2; '
+    FROM INFORMATION_SCHEMA.TABLES t
+    INNER JOIN INFORMATION_SCHEMA.COLUMNS c ON t.TABLE_NAME = c.TABLE_NAME AND t.TABLE_SCHEMA = c.TABLE_SCHEMA
+    WHERE t.TABLE_TYPE = 'BASE TABLE'
+        AND c.DATA_TYPE IN ('char', 'varchar', 'nchar', 'nvarchar', 'uniqueidentifier')
+        AND OBJECTPROPERTY(OBJECT_ID(QUOTENAME(t.TABLE_SCHEMA) + '.' + QUOTENAME(t.TABLE_NAME)), 'IsMSShipped') = 0;
 
-                DECLARE @TableName nvarchar(256), @ColumnName nvarchar(128), @SearchStr2 nvarchar(110)
-                SET  @TableName = ''
-                SET @SearchStr2 = QUOTENAME('%' + @SearchStr + '%','''')
+    -- Get the total number of tables
+    DECLARE @TotalTables INT = (SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE');
 
-                WHILE @TableName IS NOT NULL
-                BEGIN
-                                SET @ColumnName = ''
-                                SET @TableName = 
-                                (
-                                                SELECT MIN(QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME))
-                                                FROM   INFORMATION_SCHEMA.TABLES
-                                                WHERE                                 TABLE_TYPE = 'BASE TABLE'
-                                                                AND       QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME) > @TableName
-                                                                AND       OBJECTPROPERTY(
-                                                                                                OBJECT_ID(
-                                                                                                                QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME)
-                                                                                                                 ), 'IsMSShipped'
-                                                                                                       ) = 0
-                                )
+    -- Initialize the counter variables
+    DECLARE @Counter INT = 0, @Progress INT = 0;
 
-                                WHILE (@TableName IS NOT NULL) AND (@ColumnName IS NOT NULL)
-                                BEGIN
-                                                SET @ColumnName =
-                                                (
-                                                                SELECT MIN(QUOTENAME(COLUMN_NAME))
-                                                                FROM   INFORMATION_SCHEMA.COLUMNS
-                                                                WHERE                                 TABLE_SCHEMA               = PARSENAME(@TableName, 2)
-                                                                                AND       TABLE_NAME    = PARSENAME(@TableName, 1)
-                                                                                AND       DATA_TYPE IN ('char', 'varchar', 'nchar', 'nvarchar', 'uniqueidentifier')
-                                                                                AND       QUOTENAME(COLUMN_NAME) > @ColumnName
-                                                )
-                
-                                                IF @ColumnName IS NOT NULL
-                                                BEGIN
-                                                                INSERT INTO #Results
-                                                                EXEC
-                                                                (
-                                                                                'SELECT ''' + @TableName + '.' + @ColumnName + ''', LEFT(' + @ColumnName + ', 3630) 
-                                                                                FROM ' + @TableName + ' (NOLOCK) ' +
-                                                                                ' WHERE ' + @ColumnName + ' LIKE ' + @SearchStr2
-                                                
-                                                                )
-                                                                --Print @TableName
-                                                END
-                                END       
-                END
+    -- Execute the dynamic SQL statement using sp_executesql with the search string parameter
+    EXEC sp_executesql @DynamicSQL, N'@SearchStr2 NVARCHAR(110)', @SearchStr2 = @SearchStr2;
 
-                SELECT ColumnName, ColumnValue FROM #Results
+    -- Select the search results from the temporary table
+    SELECT TableName, ColumnName, ColumnValue, RowNumber FROM #Results;
+
+    -- Drop the temporary table
+    DROP TABLE #Results;
 END
