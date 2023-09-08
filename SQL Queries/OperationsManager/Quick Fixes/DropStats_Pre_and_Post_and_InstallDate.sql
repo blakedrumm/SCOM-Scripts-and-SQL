ff031817ce63003ab257b817042e714a2aa0dad8
Blake Drumm <blakedrumm@microsoft.com>
2023-09-08 10:37:06 -0400

    Update query :new:

--- SQL Queries/OperationsManager/Quick Fixes/DropStats_Pre_and_Post_and_InstallDate.sql
+++ SQL Queries/OperationsManager/Quick Fixes/DropStats_Pre_and_Post_and_InstallDate.sql
@@ -1,7 +1,11 @@
 DECLARE @sql NVARCHAR(MAX) = '';
 
+ 
+
 SELECT @sql += 'DROP STATISTICS ' + QUOTENAME(OBJECT_SCHEMA_NAME(s.object_id)) + '.' + QUOTENAME(OBJECT_NAME(s.object_id)) + '.' + QUOTENAME(s.name) + ';' + CHAR(13)
 FROM sys.stats s
-WHERE s.name LIKE 'PRE\_%' ESCAPE '\' OR s.name LIKE 'POST\_%' ESCAPE '\';
+WHERE s.name LIKE 'PRE\_%' ESCAPE '\' OR s.name LIKE 'POST\_%' ESCAPE '\' OR s.name LIKE 'InstallDate\_%' ESCAPE '\';
+
+ 
 
 EXEC sp_executesql @sql;
