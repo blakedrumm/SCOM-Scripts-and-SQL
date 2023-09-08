12ca22c984c440003e7be87004abc898391df66d
Blake Drumm <blakedrumm@microsoft.com>
2023-09-08 10:28:17 -0400

    Add query to drop stats for Pre_ and Post_ :squid:

---
+++ SQL Queries/OperationsManager/Quick Fixes/DropStats_Pre_and_Post.sql
@@ -0,0 +1,7 @@
+DECLARE @sql NVARCHAR(MAX) = '';
+
+SELECT @sql += 'DROP STATISTICS ' + QUOTENAME(OBJECT_SCHEMA_NAME(s.object_id)) + '.' + QUOTENAME(OBJECT_NAME(s.object_id)) + '.' + QUOTENAME(s.name) + ';' + CHAR(13)
+FROM sys.stats s
+WHERE s.name LIKE 'PRE\_%' ESCAPE '\' OR s.name LIKE 'POST\_%' ESCAPE '\';
+
+EXEC sp_executesql @sql;
