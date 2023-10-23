1c87607aa573cd7575d17c714238f0a39d6a9648
Blake Drumm <blakedrumm@microsoft.com>
2023-10-23 15:41:56 -0400

    Added ability to gather more scripts from Management Packs :window:

--- Powershell/Export-ScriptsFromMPXML.ps1
+++ Powershell/Export-ScriptsFromMPXML.ps1
@@ -3,7 +3,7 @@
 		Dump Scripts from inside of XML Management Packs for System Center Operations Manager
 	
 	.DESCRIPTION
-		Export Scripts from inside of XML Management Packs. This will make it easier to troubleshoot issues with scripts in Discoveries, Monitors, 
+		Export Scripts from inside of XML Management Packs. This will make it easier to troubleshoot issues with scripts in Discoveries, Monitors, or Rules.
 	
 	.PARAMETER SourceXML
 		The location of the XML Management Pack file that contains scripts you would like to export.
@@ -12,236 +12,302 @@
 		The location of Folder you would like to exports scripts contained inside the Management Pack XML file.
 	
 	.EXAMPLE
-			PS C:\> .\Export-ScriptsFromMPXML.ps1 -SourceXML C:\Temp\ManagementPack.xml -ExportLocation C:\Temp\MPScripts
+		PS C:\> .\Export-ScriptsFromMPXML.ps1 -SourceXML C:\Temp\ManagementPack.xml -ExportLocation C:\Temp\MPScripts
 	
 	.NOTES
 		Original Author: Udish Mudiar
 		Original Location: https://github.com/Udish17/SCOM-PowerShell-Scripts/blob/SCOM-PowerShell-Scripts/master/DumpScriptsfromSCOMMP.ps1
+  		Author: Blake Drumm (blakedrumm@microsoft.com)
+    		This script location: https://github.com/blakedrumm/SCOM-Scripts-and-SQL/blob/master/Powershell/Export-ScriptsFromMPXML.ps1
+
+      		Last Modified: October 23rd, 2023
 #>
+
 [CmdletBinding()]
 [OutputType([string])]
 param
 (
-	[Parameter(Mandatory = $true,
-			   Position = 0,
+	[Parameter(Position = 0,
 			   HelpMessage = 'The location of the XML Management Pack file that contains scripts you would like to export.')]
 	[Alias('input')]
 	[String[]]$SourceXML,
-	[Parameter(Mandatory = $true,
-			   Position = 1,
+	[Parameter(Position = 1,
 			   HelpMessage = 'The location you would like to exports scripts contained inside the Management Pack XML file.')]
 	[Alias('outputdirectory')]
 	[String[]]$ExportLocation
 )
 
-[xml]$xmlcontent = Get-Content $SourceXML
-
-#region Dump-DS-ProbeAction
-#This will dump only the scripts which are defined inside Data Source ProbeAction
-$probeactions = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/DataSourceModuleType/ModuleImplementation/Composite/MemberModules/ProbeAction')
-if ($probeactions)
+function Export-ScriptsFromMPXML
 {
-	foreach ($probeaction in $probeactions)
+	[CmdletBinding()]
+	[OutputType([string])]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				   Position = 0,
+				   HelpMessage = 'The location of the XML Management Pack file that contains scripts you would like to export.')]
+		[Alias('input')]
+		[String[]]$SourceXML,
+		[Parameter(Mandatory = $true,
+				   Position = 1,
+				   HelpMessage = 'The location you would like to exports scripts contained inside the Management Pack XML file.')]
+		[Alias('outputdirectory')]
+		[String[]]$ExportLocation
+	)
+	
+	[xml]$xmlcontent = Get-Content $SourceXML
+	
+	#region Dump-DS-ProbeAction
+	#This will dump only the scripts which are defined inside Data Source ProbeAction
+	$probeactions = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/DataSourceModuleType/ModuleImplementation/Composite/MemberModules/ProbeAction')
+	if ($probeactions)
 	{
-		
-		if ($probeaction.TypeID -match "PowerShell")
+		foreach ($probeaction in $probeactions)
 		{
-			$scriptname = $probeaction.ScriptName
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			#write this condition because in some case the developers has added the script extension in the script name itself
 			
-			if ($scriptname -match ".ps1")
+			if ($probeaction.TypeID -match "PowerShell")
 			{
+				$scriptname = $probeaction.ScriptName
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				#write this condition because in some case the developers has added the script extension in the script name itself
 				
-				$probeaction.scriptbody | Out-File $ExportLocation\$scriptname
+				if ($scriptname -match ".ps1")
+				{
+					
+					$probeaction.scriptbody | Out-File $ExportLocation\$scriptname
+				}
+				elseif ($scriptname -match ".vbs")
+				{
+					$probeaction.scriptbody | Out-File $ExportLocation\$scriptname
+				}
+				elseif ($scriptname -match ".js")
+				{
+					$probeaction.scriptbody | Out-File $ExportLocation\$scriptname
+				}
+				else
+				{
+					$scripttype = $probeaction.ID + "1"
+					$probeaction.scriptbody | Out-File $ExportLocation\$scriptname.$scripttype
+				}
 			}
-			elseif ($scriptname -match ".vbs")
-			{
-				$probeaction.scriptbody | Out-File $ExportLocation\$scriptname
-			}
-			elseif ($scriptname -match ".js")
-			{
-				$probeaction.scriptbody | Out-File $ExportLocation\$scriptname
-			}
-			else
-			{
-				$scripttype = $probeaction.ID + "1"
-				$probeaction.scriptbody | Out-File $ExportLocation\$scriptname.$scripttype
-			}
-		}
-		
-	}
-}
-#endregion Dump-DS-ProbeAction
-
-#region Dump-DS-Files
-#This script will dump only the scripts which are defined inside Data Source Files/File
-$files = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/DataSourceModuleType/ModuleImplementation/Composite/MemberModules/DataSource/Files/File')
-
-if ($files)
-{
-	foreach ($file in $files)
-	{
-		$scriptname = $file.name
-		if ($scriptname -match "ps1")
-		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$file.Contents | Out-File $ExportLocation\$scriptname
-		}
-		if ($scriptname -match "cmd")
-		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$file.Contents | Out-File $ExportLocation\$scriptname
-		}
-		if ($scriptname -match "vbs")
-		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$file.Contents | Out-File $ExportLocation\$scriptname
+			
 		}
 	}
-}
-#endregion Dump-DS-Files
-
-#region Dump-DS-WriteAction
-#This script will dump only the scripts which are defined inside WriteAction 
-$writeactions = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/WriteActionModuleType/ModuleImplementation/Composite/MemberModules/WriteAction')
-if ($writeactions)
-{
-	foreach ($writeaction in $writeactions)
+	#endregion Dump-DS-ProbeAction
+	
+	#region Dump-DS-Files
+	#This script will dump only the scripts which are defined inside Data Source Files/File
+	$files = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/DataSourceModuleType/ModuleImplementation/Composite/MemberModules/DataSource/Files/File')
+	
+	if ($files)
 	{
-		$scriptname = $writeaction.ScriptName
-		if ($writeaction.TypeID -match "script")
+		foreach ($file in $files)
 		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$writeaction.scriptbody | Out-File $ExportLocation\$scriptname
-		}
-		
-		
-	}
-}
-#endregion Dump-DS-WriteAction
-
-#region Dump-DS-DataSource
-#This script will dump only the scripts which are defined inside Data Source Files/File
-$datasources = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/DataSourceModuleType/ModuleImplementation/Composite/MemberModules/DataSource')
-
-if ($datasources)
-{
-	foreach ($datasource in $datasources)
-	{
-		if ($datasource.TypeID -match "Script" -and $datasource.ScriptName -ne $null)
-		{
-			$scriptname = $datasource.ScriptName
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$writeaction.scriptbody | Out-File $ExportLocation\$scriptname
-		}
-	}
-}
-
-
-
-#endregion Dump-DS-DataSource
-
-#region Dump-DS-ProbeAction-Files
-#This will dump only the scripts which are defined inside Data Source ProbeAction
-$probeactionfiles = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/DataSourceModuleType/ModuleImplementation/Composite/MemberModules/ProbeAction/Files/File')
-if ($probeactionfiles)
-{
-	foreach ($probeactionfile in $probeactionfiles)
-	{
-		$scriptname = $probeactionfile.name
-		if ($scriptname -match "ps1")
-		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$probeactionfile.Contents | Out-File $ExportLocation\$scriptname
-		}
-		elseif ($scriptname -match "cmd")
-		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$probeactionfile.Contents | Out-File $ExportLocation\$scriptname
-		}
-		elseif ($scriptname -match "vbs")
-		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$probeactionfile.Contents | Out-File $ExportLocation\$scriptname
-		}
-	}
-}
-
-#endregion Dump-DS-ProbeAction-Files
-
-#region Dump-PA-ProbeAction
-#This will dump only the scripts which are defined inside Probe Action Module Type
-$probeactionmodules = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/ProbeActionModuleType/ModuleImplementation/Composite/MemberModules/ProbeAction')
-if ($probeactionmodules)
-{
-	foreach ($probeactionmodule in $probeactionmodules)
-	{
-		if ($probeactionmodule.TypeID -match "Script")
-		{
-			$scriptname = $probeactionmodule.ScriptName
-			if ($scriptname -match "/")
-			{
-				Write-Host "Script found. But not dumping script as the script name is passed as a config item....    " -ForegroundColor Cyan
-				
-			}
-			else
+			$scriptname = $file.name
+			if ($scriptname -match "ps1")
 			{
 				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-				$probeactionmodule.scriptbody | Out-File $ExportLocation\$scriptname
+				$file.Contents | Out-File $ExportLocation\$scriptname
+			}
+			if ($scriptname -match "cmd")
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$file.Contents | Out-File $ExportLocation\$scriptname
+			}
+			if ($scriptname -match "vbs")
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$file.Contents | Out-File $ExportLocation\$scriptname
 			}
 		}
 	}
-}
-#endregion Dump-PA-ProbeAction
-
-#region Dump-Discovery-DS-Files
-#This will dump only the scripts which are defined inside Discovery Data Source Files
-$discoverydatasourcefiles = $xmlcontent.selectnodes('//ManagementPack/Monitoring/Discoveries/Discovery/DataSource/Files/File')
-if ($discoverydatasourcefiles)
-{
-	foreach ($discoverydatasourcefile in $discoverydatasourcefiles)
+	#endregion Dump-DS-Files
+	
+	#region Dump-DS-WriteAction
+	#This script will dump only the scripts which are defined inside WriteAction 
+	$writeactions = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/WriteActionModuleType/ModuleImplementation/Composite/MemberModules/WriteAction')
+	if ($writeactions)
 	{
-		$scriptname = $discoverydatasourcefile.name
-		if ($scriptname -match "ps1")
+		foreach ($writeaction in $writeactions)
 		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$discoverydatasourcefile.Contents | Out-File $ExportLocation\$scriptname
-		}
-		elseif ($scriptname -match "cmd")
-		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$discoverydatasourcefile.Contents | Out-File $ExportLocation\$scriptname
-		}
-		if ($scriptname -match "vbs")
-		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$discoverydatasourcefile.Contents | Out-File $ExportLocation\$scriptname
+			$scriptname = $writeaction.ScriptName
+			if ($writeaction.TypeID -match "script")
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$writeaction.scriptbody | Out-File $ExportLocation\$scriptname
+			}
+			
+			
 		}
 	}
-}
-#endregion Dump-Discovery-DS-Files
-
-#region Dump-Monitor-Script
-$unitmonitorconfigurations = $xmlcontent.selectnodes('//ManagementPack/Monitoring/Monitors/UnitMonitor/Configuration')
-if ($unitmonitorconfigurations)
-{
-	foreach ($unitmonitorconfiguration in $unitmonitorconfigurations)
+	#endregion Dump-DS-WriteAction
+	
+	#region Dump-Scripts
+	#This script will dump only the scripts which are defined inside Data Source Files/File
+	$datasourcesScripts = (Select-Xml -Xml $xmlcontent -XPath //*/DataSource).Node | Select PsScriptName, PowerShellScriptParameters, PowerShellScriptParam, PowerShellScriptBody, cScriptName, cScriptParameters, cScriptBody | Where-Object { $_.PsScriptName -ne $null -or $_.cScriptName -ne $null }
+	if ($datasourcesScripts)
 	{
-		$scriptname = $unitmonitorconfiguration.scriptname
-		if ($scriptname)
+		foreach ($datasource in $datasourcesScripts)
 		{
-			Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
-			$unitmonitorconfiguration.scriptbody | Out-File $ExportLocation\$scriptname
+			if ($datasource.PsScriptName)
+			{
+				$scriptname = $datasource.PsScriptName
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$powershellBody = @"
+`#$($datasource.PowerShellScriptParameters)
+$($datasource.PowerShellScriptParam)
+$($datasource.PowerShellScriptBody)
+"@
+				
+				
+				$powershellBody | Out-File $ExportLocation\$scriptname
+				
+			}
+			if ($datasource.cScriptName)
+			{
+				$scriptname = $datasource.cScriptName
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$cscriptBody = @"
+$($datasource.cScriptParameters)
+$($datasource.cScriptBody)
+"@
+				
+				
+				$cscriptBody | Out-File $ExportLocation\$scriptname
+			}
 		}
 	}
+	#endregion Dump-Scripts
+	
+	#region Dump-DS-DataSource
+	#This script will dump only the scripts which are defined inside Data Source Files/File
+	$datasources = $xmlcontent.SelectNodes("//ManagementPack/TypeDefinitions/ModuleTypes/*[starts-with(name(), 'DataSourceModuleType')]/ModuleImplementation/Composite/MemberModules/*[starts-with(name(), 'DataSource')]")
+	if ($datasources)
+	{
+		foreach ($datasource in $datasources)
+		{
+			if ($datasource.TypeID -match "Script" -and $datasource.ScriptName -ne $null)
+			{
+				$scriptname = $datasource.ScriptName
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$writeaction.scriptbody | Out-File $ExportLocation\$scriptname
+			}
+		}
+	}
+	
+	
+	
+	#endregion Dump-DS-DataSource
+	
+	#region Dump-DS-ProbeAction-Files
+	#This will dump only the scripts which are defined inside Data Source ProbeAction
+	$probeactionfiles = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/DataSourceModuleType/ModuleImplementation/Composite/MemberModules/ProbeAction/Files/File')
+	if ($probeactionfiles)
+	{
+		foreach ($probeactionfile in $probeactionfiles)
+		{
+			$scriptname = $probeactionfile.name
+			if ($scriptname -match "ps1")
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$probeactionfile.Contents | Out-File $ExportLocation\$scriptname
+			}
+			elseif ($scriptname -match "cmd")
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$probeactionfile.Contents | Out-File $ExportLocation\$scriptname
+			}
+			elseif ($scriptname -match "vbs")
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$probeactionfile.Contents | Out-File $ExportLocation\$scriptname
+			}
+		}
+	}
+	
+	#endregion Dump-DS-ProbeAction-Files
+	
+	#region Dump-PA-ProbeAction
+	#This will dump only the scripts which are defined inside Probe Action Module Type
+	$probeactionmodules = $xmlcontent.selectnodes('//ManagementPack/TypeDefinitions/ModuleTypes/ProbeActionModuleType/ModuleImplementation/Composite/MemberModules/ProbeAction')
+	if ($probeactionmodules)
+	{
+		foreach ($probeactionmodule in $probeactionmodules)
+		{
+			if ($probeactionmodule.TypeID -match "Script")
+			{
+				$scriptname = $probeactionmodule.ScriptName
+				if ($scriptname -match "/")
+				{
+					Write-Host "Script found. But not dumping script as the script name is passed as a config item....    " -ForegroundColor Cyan
+					
+				}
+				else
+				{
+					Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+					$probeactionmodule.scriptbody | Out-File $ExportLocation\$scriptname
+				}
+			}
+		}
+	}
+	#endregion Dump-PA-ProbeAction
+	
+	#region Dump-Discovery-DS-Files
+	#This will dump only the scripts which are defined inside Discovery Data Source Files
+	$discoverydatasourcefiles = $xmlcontent.selectnodes('//ManagementPack/Monitoring/Discoveries/Discovery/DataSource/Files/File')
+	if ($discoverydatasourcefiles)
+	{
+		foreach ($discoverydatasourcefile in $discoverydatasourcefiles)
+		{
+			$scriptname = $discoverydatasourcefile.name
+			if ($scriptname -match "ps1")
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$discoverydatasourcefile.Contents | Out-File $ExportLocation\$scriptname
+			}
+			elseif ($scriptname -match "cmd")
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$discoverydatasourcefile.Contents | Out-File $ExportLocation\$scriptname
+			}
+			if ($scriptname -match "vbs")
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$discoverydatasourcefile.Contents | Out-File $ExportLocation\$scriptname
+			}
+		}
+	}
+	#endregion Dump-Discovery-DS-Files
+	
+	#region Dump-Monitor-Script
+	$unitmonitorconfigurations = $xmlcontent.selectnodes('//ManagementPack/Monitoring/Monitors/UnitMonitor/Configuration')
+	if ($unitmonitorconfigurations)
+	{
+		foreach ($unitmonitorconfiguration in $unitmonitorconfigurations)
+		{
+			$scriptname = $unitmonitorconfiguration.scriptname
+			if ($scriptname)
+			{
+				Write-Host "Script found. Dumping Script....    " $scriptname -ForegroundColor Green
+				$unitmonitorconfiguration.scriptbody | Out-File $ExportLocation\$scriptname
+			}
+		}
+	}
+	#endregion Dump-Monitor-Script
+	
+	#region NoDump
+	$content = Get-ChildItem $ExportLocation
+	if (!$content)
+	{
+		Write-Host "No scripts were dumped for the Management Pack supplied: $SourceXML" -ForegroundColor Cyan
+	}
+	#endregion NoDump
 }
-#endregion Dump-Monitor-Script
-
-#region NoDump
-$content = Get-ChildItem $ExportLocation
-if (!$content)
+if ($SourceXML -or $ExportLocation)
 {
-	Write-Host "No scripts were dumped for the Management Pack supplied: $SourceXML" -ForegroundColor Cyan
+	Export-ScriptsFromMPXML -SourceXML $SourceXML -ExportLocation $ExportLocation
 }
-#endregion NoDump
+else
+{
+	Export-ScriptsFromMPXML
+}
