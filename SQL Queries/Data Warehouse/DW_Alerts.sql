e43bb4c7f89e80ef77ce014f6205bd74b1abbb49
Blake Drumm <blakedrumm@microsoft.com>
2023-10-31 12:14:57 -0400

    Add comment for WHERE :+1:

--- SQL Queries/Data Warehouse/DW_Alerts.sql
+++ SQL Queries/Data Warehouse/DW_Alerts.sql
@@ -9,5 +9,5 @@ Select         A.[RaisedDateTime],
                A.[RepeatCount] as Repeat_Count
 From [Alert].[vAlert] as A
 Join [dbo].[vManagedEntity] As M On A.ManagedEntityRowId=M.ManagedEntityRowId
-WHERE RaisedDateTime BETWEEN GETDATE()-60 AND GETDATE()
+--WHERE RaisedDateTime BETWEEN GETDATE()-60 AND GETDATE()
 Order by RaisedDateTime Desc
