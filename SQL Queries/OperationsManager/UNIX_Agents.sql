216db5d48d8f83d47ec5ae72881dca13f202b714
Blake Drumm <blakedrumm@microsoft.com>
2023-11-10 17:40:25 -0500

    Updated the `Unix_Agents.sql` command to include the IsDeleted, TimeAdded, and LastModified columns :building_construction:

--- SQL Queries/OperationsManager/UNIX_Agents.sql
+++ SQL Queries/OperationsManager/UNIX_Agents.sql
@@ -9,18 +9,17 @@ and COLUMN_NAME Like 'Architecture%'
 SELECT @IPAddressCol=COLUMN_NAME FROM INFORMATION_SCHEMA.Columns WHERE TABLE_NAME = 'MT_Microsoft$Unix$Computer'
 AND COLUMN_NAME Like 'IPAddress%'
 set @query = 'select bme2.fullname as ''FullName'', bme2.Displayname as ''DisplayName'', ' + @AgentVersionCol + '  as ''Build'' ,
-' + @ArchitectureCol + ' as ''Architecture'', ' + @IPAddressCol + ' as ''IPAddress'', bme.Displayname as ''ResourcePool''
+' + @ArchitectureCol + ' as ''Architecture'', ' + @IPAddressCol + ' as ''IPAddress'', bme.Displayname as ''ResourcePool'', bme.IsDeleted, bme.TimeAdded, bme.LastModified
 from dbo.Relationship r with (nolock) 
-join dbo.RelationshipType rt with (nolock) 
+LEFT join dbo.RelationshipType rt with (nolock) 
 on r.RelationshipTypeId = rt.RelationshipTypeId 
-join dbo.BasemanagedEntity bme with (nolock) 
+LEFT join dbo.BasemanagedEntity bme with (nolock) 
 on bme.basemanagedentityid = r.SourceEntityId 
-join dbo.BasemanagedEntity bme2 with (nolock) 
+LEFT join dbo.BasemanagedEntity bme2 with (nolock) 
 on r.TargetEntityId = bme2.BaseManagedEntityId 
-join MT_Microsoft$Unix$Computer mtvc
+LEFT join MT_Microsoft$Unix$Computer mtvc
 on bme2.BaseManagedEntityId = mtvc.BaseManagedEntityId
 where rt.RelationshipTypeName = ''Microsoft.SystemCenter.ManagementActionPointManagesEntity ''  
-and bme.IsDeleted = 0 
 and r.IsDeleted = 0 
 and bme2.basemanagedtypeid in (SELECT DerivedTypeId 
 FROM DerivedManagedTypes with (nolock) 
