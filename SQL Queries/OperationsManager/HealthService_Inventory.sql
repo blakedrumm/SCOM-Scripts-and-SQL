73f534d1d44281338d0fe6f15c0ed00ae5dc3ee0
Blake Drumm <blakedrumm@microsoft.com>
2023-11-10 17:45:12 -0500

    Update HealthService_Inventory.sql :waffle:

--- SQL Queries/OperationsManager/HealthService_Inventory.sql
+++ SQL Queries/OperationsManager/HealthService_Inventory.sql
@@ -1,25 +1,18 @@
-SELECT HS.BaseManagedEntityId,
-HS.DisplayName AS HealthService,
-TargetBME.DisplayName AS PrimaryManagementServer,
-Version, 
-ActionAccountIdentity, 
-ActiveDirectoryManaged,
-CreateListener,
-ProxyingEnabled, 
-HeartbeatEnabled,
-HeartbeatInterval, 
-IsManuallyInstalled, 
-InstallTime,
-Port,
-ProxyingEnabled,
-IsAgent, 
-IsGateway, 
-IsManagementServer,
-IsRHS,
-PatchList,
-MaximumQueueSize
-FROM MTV_HealthService HS WITH (NOLOCK)
-JOIN Relationship R WITH (NOLOCK) ON R.SourceEntityId = HS.BaseManagedEntityId
-JOIN BaseManagedEntity TargetBME WITH (NOLOCK) ON R.TargetEntityId = TargetBME.BaseManagedEntityId
-WHERE R.IsDeleted = 0 AND R.RelationshipTypeId  = dbo.fn_ManagedTypeId_MicrosoftSystemCenterHealthServiceCommunication()
-ORDER BY [HealthService],[PrimaryManagementServer]
+SELECT 
+    TargetBME.DisplayName AS PrimaryManagementServer,
+    OS.DisplayName AS OSVersion,
+	OS.OSVersion_53D6DEB6_BE2E_D1B6_D49E_A623518BD867 AS OSBuildNumber,
+    HS.*
+FROM 
+    MTV_HealthService HS WITH (NOLOCK)
+LEFT JOIN 
+    Relationship R WITH (NOLOCK) ON R.SourceEntityId = HS.BaseManagedEntityId
+LEFT JOIN 
+    BaseManagedEntity TargetBME WITH (NOLOCK) ON R.TargetEntityId = TargetBME.BaseManagedEntityId
+LEFT JOIN 
+    MTV_Microsoft$Windows$OperatingSystem OS WITH (NOLOCK) ON HS.DisplayName = OS.PrincipalName -- Join with the OS version table
+WHERE 
+    R.IsDeleted = 0 
+    AND R.RelationshipTypeId = dbo.fn_ManagedTypeId_MicrosoftSystemCenterHealthServiceCommunication()
+ORDER BY 
+    [DisplayName], [PrimaryManagementServer];
