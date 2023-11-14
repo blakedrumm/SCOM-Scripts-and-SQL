00f9b187ab51d9f4b8e9d448b0f11e98d3d81425
Blake Drumm <blakedrumm@microsoft.com>
2023-11-13 23:36:36 -0500

    Update to working version :bell:

--- Powershell/Invoke-SCXWinRMEnumeration.ps1
+++ Powershell/Invoke-SCXWinRMEnumeration.ps1
@@ -1,3 +1,46 @@
+<#
+.SYNOPSIS
+    Invoke-SCXWinRMEnumeration - Enumerates various SCX (System Center Cross Platform) classes on specified servers using Windows Remote Management (WinRM).
+
+.DESCRIPTION
+    This script is designed to enumerate a set of SCX classes on one or more servers using WinRM. It supports both Basic and Kerberos authentication methods. The script is useful for querying and collecting information from remote servers about various system components like disk drives, filesystems, memory, processors, etc.
+
+.PARAMETER Servers
+    An array of server names or IP addresses on which the SCX class enumeration will be performed. This parameter is mandatory.
+
+.PARAMETER Username
+    The username to be used for authentication on the target server(s). It is not mandatory, but if specified without a password, the script will prompt for the password.
+
+.PARAMETER Password
+    The password for the provided username. If the Basic authentication method is chosen, the password is mandatory.
+
+.PARAMETER AuthenticationMethod
+    The authentication method to be used. Valid options are "Basic" and "Kerberos". The default is "Basic". If Kerberos is chosen, ensure that Kerberos tickets are available or the Kerberos setup is appropriately configured.
+
+.EXAMPLE
+    PS> .\Invoke-SCXWinRMEnumeration.ps1 -Servers "Server1","Server2" -Username "admin" -Password "password" -AuthenticationMethod "Basic"
+    This command enumerates SCX classes on Server1 and Server2 using Basic authentication with the provided username and password.
+
+.EXAMPLE
+    PS> .\Invoke-SCXWinRMEnumeration.ps1 -Servers "Server3" -Username "admin" -AuthenticationMethod "Kerberos"
+    This command enumerates SCX classes on Server3 using Kerberos authentication. The script assumes Kerberos is configured and tickets are available.
+
+.NOTES
+    Author: Blake Drumm (blakedrumm@microsoft.com)
+    Website: https://blakedrumm.com/
+    Version: 1.0
+    Created: November 13, 2023
+    Requirements: PowerShell 5.0 or later, WinRM must be configured on the target server(s).
+
+#>
+param (
+    [string[]]$Servers,
+    [string]$Username,
+    [string]$Password,
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Basic", "Kerberos")]
+    [string]$AuthenticationMethod = "Basic"
+)
 function Invoke-SCXWinRMEnumeration {
     param (
         [Parameter(Mandatory = $true)]
@@ -30,6 +73,12 @@ function Invoke-SCXWinRMEnumeration {
 
     }
 }
-
-# Usage example
-Invoke-SCXWinRMEnumeration -Servers 'rhel7-9.contoso-2019.com' -Username 'testuser' -Password 'Password1' -AuthenticationMethod 'Basic'
+if ($Servers -or $Username -or $Password)
+{
+    Invoke-SCXWinRMEnumeration -Servers $Servers -Username $Username -Password $Password -AuthenticationMethod $AuthenticationMethod
+}
+else
+{
+    # Usage example
+    Invoke-SCXWinRMEnumeration -Servers 'rhel7-9.contoso-2019.com' -Username 'monuser' -Password 'Thenewme1234' -AuthenticationMethod 'Basic'
+}
