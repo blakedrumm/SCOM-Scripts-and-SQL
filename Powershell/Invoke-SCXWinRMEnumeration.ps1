66e366a259c48613e6b1ac3fd37012da42709e97
Blake Drumm <blakedrumm@microsoft.com>
2023-11-13 20:57:40 -0500

    Update to old script :scroll:

--- Powershell/Invoke-SCXWinRMEnumeration.ps1
+++ Powershell/Invoke-SCXWinRMEnumeration.ps1
@@ -1,114 +1,52 @@
-<#
-.SYNOPSIS
-    Invoke-SCXWinRMEnumeration - Enumerates various SCX (System Center Cross Platform) classes on specified servers using Windows Remote Management (WinRM).
+function Invoke-WinRMEnumeration {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$ServerName,
 
-.DESCRIPTION
-    This script is designed to enumerate a set of SCX classes on one or more servers using WinRM. It supports both Basic and Kerberos authentication methods. The script is useful for querying and collecting information from remote servers about various system components like disk drives, filesystems, memory, processors, etc.
+        [Parameter(Mandatory=$true)]
+        [string]$Username,
 
-.PARAMETER Servers
-    An array of server names or IP addresses on which the SCX class enumeration will be performed. This parameter is mandatory.
+        [Parameter(Mandatory=$true)]
+        [string]$Password,
 
-.PARAMETER Username
-    The username to be used for authentication on the target server(s). It is not mandatory, but if specified without a password, the script will prompt for the password.
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("Basic", "Kerberos")]
+        [string]$AuthenticationMethod = "Basic"
+    )
 
-.PARAMETER Password
-    The password for the provided username. If the Basic authentication method is chosen, the password is mandatory.
+    $baseUri = "http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/"
+    $cimNamespace = "?__cimnamespace=root/scx"
+    $endpoint = "https://$ServerName`:1270/wsman"
+    $scxClasses = @(
+        "SCX_Agent", 
+        "SCX_DiskDrive", 
+        "SCX_DiskDriveStatisticalInformation", 
+        "SCX_EthernetPortStatistics", 
+        "SCX_FileSystem", 
+        "SCX_FileSystemStatisticalInformation", 
+        "SCX_IPProtocolEndpoint", 
+        "SCX_LogFile", 
+        "SCX_MemoryStatisticalInformation", 
+        "SCX_OperatingSystem", 
+        "SCX_ProcessorStatisticalInformation", 
+        "SCX_StatisticalInformation", 
+        "SCX_UnixProcess", 
+        "SCX_UnixProcessStatisticalInformation",
+        "SCX_Application_Server"
+    )
 
-.PARAMETER AuthenticationMethod
-    The authentication method to be used. Valid options are "Basic" and "Kerberos". The default is "Basic". If Kerberos is chosen, ensure that Kerberos tickets are available or the Kerberos setup is appropriately configured.
+    foreach ($class in $scxClasses) {
+        $uri = $baseUri + $class + $cimNamespace
 
-.EXAMPLE
-    PS> .\Invoke-SCXWinRMEnumeration.ps1 -Servers "Server1","Server2" -Username "admin" -Password "password" -AuthenticationMethod "Basic"
-    This command enumerates SCX classes on Server1 and Server2 using Basic authentication with the provided username and password.
+        if ($AuthenticationMethod -eq "Basic") {
+            $command = "winrm enumerate $uri -username:$Username -password:$Password -r:$endpoint -auth:Basic -skipCAcheck -skipCNcheck -skipRevocationcheck -encoding:utf-8"
+        }
+        elseif ($AuthenticationMethod -eq "Kerberos") {
+            $command = "winrm e $uri -r:$endpoint -u:$Username -p:$Password -auth:Kerberos -skipcacheck -skipcncheck -encoding:utf-8"
+        }
 
-.EXAMPLE
-    PS> .\Invoke-SCXWinRMEnumeration.ps1 -Servers "Server3" -Username "admin" -AuthenticationMethod "Kerberos"
-    This command enumerates SCX classes on Server3 using Kerberos authentication. The script assumes Kerberos is configured and tickets are available.
-
-.NOTES
-    Author: Blake Drumm (blakedrumm@microsoft.com)
-    Website: https://blakedrumm.com/
-    Version: 1.0
-    Created: November 13, 2023
-    Requirements: PowerShell 5.0 or later, WinRM must be configured on the target server(s).
-
-#>
-param (
-	[Parameter(Mandatory = $true)]
-	[string[]]$Servers,
-	[string]$Username,
-	[string]$Password,
-	[Parameter(Mandatory = $false)]
-	[ValidateSet("Basic", "Kerberos")]
-	[string]$AuthenticationMethod = "Basic"
-)
-
-function Invoke-SCXWinRMEnumeration
-{
-	param (
-		[Parameter(Mandatory = $true)]
-		[string[]]$Servers,
-		[string]$Username,
-		[string]$Password,
-		[Parameter(Mandatory = $false)]
-		[ValidateSet("Basic", "Kerberos")]
-		[string]$AuthenticationMethod = "Basic"
-	)
-	
-	if ($AuthenticationMethod -eq 'Basic' -and -NOT $Password)
-	{
-		Write-Warning "Missing the -Password parameter."
-		break
-	}
-	
-	$baseUri = "http://schemas.microsoft.com/wbem/wscim/1/cim-schema/2/"
-	$cimNamespace = "?__cimnamespace=root/scx"
-	
-	$scxClasses = @(
-		"SCX_Agent",
-		"SCX_DiskDrive",
-		"SCX_DiskDriveStatisticalInformation",
-		"SCX_EthernetPortStatistics",
-		"SCX_FileSystem",
-		"SCX_FileSystemStatisticalInformation",
-		"SCX_IPProtocolEndpoint",
-		"SCX_LogFile",
-		"SCX_MemoryStatisticalInformation",
-		"SCX_OperatingSystem",
-		"SCX_ProcessorStatisticalInformation",
-		"SCX_StatisticalInformation",
-		"SCX_UnixProcess",
-		"SCX_UnixProcessStatisticalInformation",
-		"SCX_Application_Server"
-	)
-	
-	foreach ($ServerName in $Servers)
-	{
-		$endpoint = "https://$ServerName`:1270/wsman"
-		
-		foreach ($class in $scxClasses)
-		{
-			$uri = $baseUri + $class + $cimNamespace
-			
-			if ($AuthenticationMethod -eq "Basic")
-			{
-				$command = "winrm enumerate $uri -username:$Username -password:$Password -r:$endpoint -auth:Basic -skipCAcheck -skipCNcheck -skipRevocationcheck -encoding:utf-8"
-			}
-			elseif ($AuthenticationMethod -eq "Kerberos")
-			{
-				$command = "winrm enumerate $uri -r:$endpoint -username:$Username -password:$Password -auth:Kerberos -skipcacheck -skipcncheck -encoding:utf-8"
-			}
-			
-			Invoke-Expression $command
-		}
-	}
+        Invoke-Expression $command
+    }
 }
 
-if ($Servers -or $Username -or $Password -or $AuthenticationMethod)
-{
-	Invoke-SCXWinRMEnumeration -Servers $Servers -Username $Username -Password $Password -AuthenticationMethod:$AuthenticationMethod
-}
-else
-{
-	Invoke-SCXWinRMEnumeration
-}
+#Invoke-WinRMEnumeration -ServerName RHEL7-9.contoso-2019.com -Username test -Password Password1 -AuthenticationMethod Basic
