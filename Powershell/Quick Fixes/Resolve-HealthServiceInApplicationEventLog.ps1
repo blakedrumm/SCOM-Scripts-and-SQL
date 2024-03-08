c679e9dbcb2f4f74dfbcaf950db2e9012217dafb
Blake Drumm <blakedrumm@microsoft.com>
2024-03-08 11:52:42 -0500

    Remove duplicate script :duck:

--- Powershell/Quick Fixes/Resolve-HealthServiceInApplicationEventLog.ps1
+++
@@ -1,7 +0,0 @@
-# Author: Blake Drumm (blakedrumm@microsoft.com)
-# Date Created: November 1st, 2023
-# ------------------------------------------------
-# Description:
-# This script will fix issues with the Health Service events showing in the Application Event Logs.
-# ------------------------------------------------
-Get-Item "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Application\HealthService" -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force; Restart-Service HealthService -Force
