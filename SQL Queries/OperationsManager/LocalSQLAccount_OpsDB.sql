269346017e7ea1a022db0fd76a21dddca26c70a3
Blake Drumm <blakedrumm@microsoft.com>
2024-03-19 01:48:28 -0400

    Add new query: `LocalSQLAccount_*.sql` :football:

---
+++ SQL Queries/OperationsManager/LocalSQLAccount_OpsDB.sql
@@ -0,0 +1,35 @@
+DECLARE @DB_Users TABLE
+(DBName sysname, UserName sysname, LoginType sysname, AssociatedRole varchar(max),create_date datetime,modify_date datetime)
+
+INSERT @DB_Users
+EXEC sp_MSforeachdb
+
+'
+use [?]
+SELECT ''?'' AS DB_Name,
+case prin.name when ''dbo'' then prin.name + '' (''+ (select SUSER_SNAME(owner_sid) from master.sys.databases where name =''?'') + '')'' else prin.name end AS UserName,
+prin.type_desc AS LoginType,
+isnull(USER_NAME(mem.role_principal_id),'''') AS AssociatedRole ,create_date,modify_date
+FROM sys.database_principals prin
+LEFT OUTER JOIN sys.database_role_members mem ON prin.principal_id=mem.member_principal_id
+WHERE prin.sid IS NOT NULL and prin.sid NOT IN (0x00) and
+prin.is_fixed_role <> 1 AND prin.name NOT LIKE ''##%'''
+
+SELECT
+dbname,username ,logintype ,create_date ,modify_date ,
+STUFF(
+(
+SELECT ',' + CONVERT(VARCHAR(500),associatedrole)
+FROM @DB_Users user2
+WHERE
+user1.DBName=user2.DBName AND user1.UserName=user2.UserName
+FOR XML PATH('')
+)
+,1,1,'') AS Permissions_user
+FROM @DB_Users user1
+WHERE LoginType = 'SQL_USER' and
+UserName != 'dbo (sa)' and
+UserName != 'MS_DataCollectorInternalUser'
+GROUP BY
+dbname,username ,logintype ,create_date ,modify_date
+ORDER BY DBName, username
