b171eb957c8a43719d873dc3bd59ac4c20f9a34e
Blake Drumm <blakedrumm@microsoft.com>
2024-05-07 16:48:51 -0400

    Add computer name to output :white_check_mark:

--- SQL Queries/Data Warehouse/DW_Events_ByNumber.sql
+++ SQL Queries/Data Warehouse/DW_Events_ByNumber.sql
@@ -1,6 +1,7 @@
---Most common events by event number and raw event description (this will take a very long time to run but it shows us not only event ID – but a description of the event to help understand which MP is the generating the noise)
-SELECT top 100 EventDisplayNumber, Rawdescription, COUNT(*) AS TotalEvents 
+--Most common events by event number and raw event description and computer name (this will take a very long time to run but it shows us not only event ID – but a description of the event to help understand which MP is the generating the noise)
+SELECT top 100 evt.EventDisplayNumber, evtd.RawDescription, evtlc.ComputerName, COUNT(*) AS TotalEvents
 FROM Event.vEvent evt 
 inner join Event.vEventDetail evtd on evt.eventoriginid = evtd.eventoriginid 
-GROUP BY EventDisplayNumber, Rawdescription 
+inner join vEventLoggingComputer evtlc on evt.LoggingComputerRowId = evtlc.EventLoggingComputerRowId
+GROUP BY evt.EventDisplayNumber, evtd.RawDescription, evtlc.ComputerName
 ORDER BY TotalEvents DESC
